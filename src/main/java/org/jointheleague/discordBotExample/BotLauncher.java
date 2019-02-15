325dfab21218ac59de7ef7c8999f3ff69d7a5987
keithagroves <keith.groves@jointheleague.org>
2019-02-15 08:21:13 -0800

    edit bot launcher

--- src/main/java/org/jointheleague/discordBotExample/BotLauncher.java
+++ src/main/java/org/jointheleague/discordBotExample/BotLauncher.java
@@ -2,12 +2,17 @@ package org.jointheleague.discordBotExample;
 
 import org.javacord.api.DiscordApi;
 import org.javacord.api.DiscordApiBuilder;
+
 /**
- * BotLauncher can launch one or more bots if needed. 
+ * BotLauncher can launch one or more bots if needed.
+ * 
  * @author keithgroves
  *
  */
 public class BotLauncher {
+	public static void main(String[] args) {
+		new BotLauncher().launch();
+	}
 
 	public void launch() {
 		System.out.println("Launching bots!");
