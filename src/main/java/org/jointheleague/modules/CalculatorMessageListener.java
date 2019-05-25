a9c6caed94509a08e7688fabcd55b9035fdc8624
soda <soda@brave-law.lan>
2019-05-25 12:42:44 -0700

    Calculator feature NOT DONE, bug: will not process input

--- src/main/java/org/jointheleague/modules/CalculatorMessageListener.java
+++ src/main/java/org/jointheleague/modules/CalculatorMessageListener.java
@@ -7,6 +7,8 @@ import org.javacord.api.event.message.MessageCreateEvent;
 
 public class CalculatorMessageListener extends CustomMessageCreateListener {
 
+	
+
 	public CalculatorMessageListener(String channelName) {
 		super(channelName);
 	}
@@ -14,40 +16,51 @@ public class CalculatorMessageListener extends CustomMessageCreateListener {
 	@Override
 	public void handle(MessageCreateEvent event) {
 		if (event.getMessageContent().startsWith("!math")) {
-			event.getChannel().sendMessage("I can do basic arithmetic operations with 2 numbers, such as: ");
-			event.getChannel().sendMessage("X * Y, X + Y, X - Y, X / Y");
-			event.getChannel().sendMessage("What do you want me to calculate?");
-
-			if (processCalc(event.getMessageContent()) != ) {
-				
+			if (event.getMessageContent().equals("!math")) {
+				event.getChannel().sendMessage("I can do basic arithmetic operations with 2 numbers, such as: ");
+				event.getChannel().sendMessage("X * Y, X + Y, X - Y, X / Y");
+				event.getChannel().sendMessage("To calculate, type !math <math operation>");
 			} else {
-				event.getChannel().sendMessage("Dude, that is not a valid arithmetic operation.");
+				String calc = event.getMessageContent().substring(6);
+				processCalc(calc, event);
 			}
 		}
 	}
 	
 	/** Processes a calculation. */
-	public Object[] processCalc(String s, MessageCreateEvent event) {
+	public void processCalc(String s, MessageCreateEvent event) {
 		String calcregex = "(\\d+) {0,1}(\\+|-|\\*|\\/) {0,1}(\\d+)";
 		
 		if (s.matches(calcregex)) {
 			Pattern calcpattern = Pattern.compile(s, Pattern.MULTILINE);
 			Matcher matcher = calcpattern.matcher(calcregex);
-			matcher.replaceAll("$0");
 			
+			int dig1 = Integer.parseInt(matcher.replaceAll("$1"));
+
+			System.out.println(dig1+"");
+			String oper = matcher.replaceAll("$2");
+
+			System.out.println(oper);
+			int dig2 = Integer.parseInt(matcher.replaceAll("$3"));
+			System.out.println(dig2+"");
+			
+			switch (oper) {
+			case "+":
+				event.getChannel().sendMessage(dig1+dig2+"");
+				break;
+			case "-":
+				event.getChannel().sendMessage(dig1-dig2+"");
+				break;
+			case "*":
+				event.getChannel().sendMessage(dig1*dig2+"");
+				break;
+			case "/":
+				event.getChannel().sendMessage(dig1/dig2+"");
+				break;
+			}
 		} else {
 			event.getChannel().sendMessage("Dude, that is not a valid arithmetic operation.");
 		}
-		
-		if (s.matches(calcregex)) {
-			Pattern calcpattern = Pattern.compile(calcregex, Pattern.MULTILINE);
-			Matcher matcher = calcpattern.matcher(calcregex);
-			
-		} else {
-			event.getChannel().sendMessage("Dude, that is not a valid arithmetic operation.");
-		}
-		
-		return null;
 	}
 
 }
