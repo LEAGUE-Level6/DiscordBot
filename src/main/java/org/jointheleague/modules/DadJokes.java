a5bd0bd56a3faea9c0ecf1d6827c81b5f9587b53
League Student <league@iMac.attlocal.net>
2019-05-26 11:05:19 -0700

    done

--- src/main/java/org/jointheleague/modules/DadJokes.java
+++ src/main/java/org/jointheleague/modules/DadJokes.java
@@ -4,9 +4,9 @@ import org.javacord.api.event.message.MessageCreateEvent;
 
 public class DadJokes extends CustomMessageCreateListener {
 
-	private static final String c1 = "im";
-	private static final String c2 = "i'm";
-	private static final String c3 = "i am";
+	 private String c1 = "im";
+	 private String c2 = "i'm";
+	 private String c3 = "i am";
 	
 
 	public DadJokes(String channelName) {
@@ -17,16 +17,16 @@ public class DadJokes extends CustomMessageCreateListener {
 	public void handle(MessageCreateEvent event) {
 		String a = event.getMessageContent();
 		
-		String s = event.getMessageContent().toLowerCase();
-		if (s.contains(c1) && !event); {
+		String s = a.toLowerCase();
+		if (s.contains(c1) && !event.getMessageAuthor().isYourself()) {
 			int x = s.indexOf(c1)+3;
 			event.getChannel().sendMessage("Hi "+a.substring(x)+", I'm Dad!");
 		}
-		if (s.contains(c2)); {
+		if (s.contains(c2) && !event.getMessageAuthor().isYourself()) {
 			int x = s.indexOf(c2)+4;
 			event.getChannel().sendMessage("Hi "+a.substring(x)+", I'm Dad!");
 		}
-		if (s.contains(c3)); {
+		if (s.contains(c3) && !event.getMessageAuthor().isYourself()) {
 			int x = s.indexOf(c3)+5;
 			event.getChannel().sendMessage("Hi "+a.substring(x)+", I'm Dad!");
 		}
