6818e4e8df7eb34f364ced2d35ec8f546cc74cbc
League Student <league@iMac-37.attlocal.net>
2019-09-01 14:26:26 -0700

    Parameter Parsing for Poll Creation (WIP)

--- src/main/java/org/jointheleague/modules/PollMessageListener.java
+++ src/main/java/org/jointheleague/modules/PollMessageListener.java
@@ -1,9 +1,14 @@
 package org.jointheleague.modules;
 
+import java.awt.Color;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
 
+import org.javacord.api.entity.message.embed.EmbedBuilder;
 import org.javacord.api.event.message.MessageCreateEvent;
 
 import com.google.gson.JsonArray;
@@ -56,9 +61,111 @@ public class PollMessageListener extends CustomMessageCreateListener {
 			newChannel.add("cPolls", new JsonArray());
 			jArr.add(newChannel);
 			channelPolls = newChannel;
+			saveJson();
 		}
 		
-		//TODO Make Poll Creation
+		String fullMessage = event.getMessageContent();
+		int commandLength = new String("!createPoll").length();
+		if(fullMessage.length() > commandLength) {
+			
+			
+			
+			String[] parameters = fullMessage.substring(commandLength).trim().split(" ");
+			
+			
+			for(int i = 0; i < parameters.length; i++) {
+				System.out.println(parameters[i]);
+			}
+			if(parameters.length >= 3) {
+				try {
+					//TODO add parameters into poll json
+					
+					
+					JsonObject newPoll = new JsonObject();
+					
+					int nextParameter = 0;
+					
+					String name = parameters[0];
+					if(parameters[0].contains("\"")) {
+						int otherQuote = -1;
+						for(int j = 1; j < parameters.length; j++) {
+							if(parameters[j].contains("\"")) {
+								otherQuote = j;
+							}
+						}
+						if(otherQuote == -1) {
+							event.getChannel().sendMessage("There was a problem reading the quotation marks");
+							return;
+						}
+						String finalName = "";
+						for(int j = 0; j <= otherQuote; j++) {
+							finalName += parameters[j];
+							if(j != otherQuote) {
+								finalName += " ";
+							}
+						}
+						
+						finalName = finalName.replaceFirst("\"", "");
+						
+						name = finalName;
+						nextParameter = otherQuote+1;
+						
+					}
+					newPoll.addProperty("name", name);
+					
+					String options = parameters[nextParameter];
+					newPoll.addProperty("options", options);
+					nextParameter++;
+					
+					String description = parameters[nextParameter];
+					if(parameters[nextParameter].contains("\"")) {
+						int otherQuote = -1;
+						for(int j = nextParameter+1; j < parameters.length; j++) {
+							if(parameters[j].contains("\"")) {
+								otherQuote = j;
+							}
+						}
+						if(otherQuote == -1) {
+							event.getChannel().sendMessage("There was a problem reading the quotation marks");
+							return;
+						}
+						String finalName = "";
+						for(int j = 0; j <= otherQuote; j++) {
+							finalName += parameters[j];
+							if(j != otherQuote) {
+								finalName += " ";
+							}
+						}
+						
+						finalName = finalName.replaceFirst("\"", "");
+						
+						description = finalName;
+						nextParameter = otherQuote+1;
+					}
+					
+					newPoll.addProperty("description", description);
+					
+					
+					event.getChannel().sendMessage(new EmbedBuilder()
+							.setTitle("Created a New Poll")
+							.setColor(Color.GREEN)
+							.addField("Name", name)
+							.addField("Description", description)
+							.addField("Options", options)
+							);
+				}
+				catch(IndexOutOfBoundsException e) {
+					event.getChannel().sendMessage(new EmbedBuilder().setTitle("Invalid Parameters (IndexOutOfBoundsException)").setDescription("!createPoll \"<Poll Name>\" <Option1,Option2,Option3,...> \"<Description>\"").setColor(Color.RED));
+				}
+				
+			}
+			else {
+				event.getChannel().sendMessage(new EmbedBuilder().setTitle("Invalid Parameters").setDescription("!createPoll \"<Poll Name>\" <Option1,Option2,Option3,...> \"<Description>\"").setColor(Color.RED));
+			}
+		}
+		else {
+			event.getChannel().sendMessage(new EmbedBuilder().setTitle("Invalid Parameters").setDescription("!createPoll \"<Poll Name>\" <Option1,Option2,Option3,...> \"<Description>\"").setColor(Color.RED));
+		}
 		
 	}
 	
@@ -79,11 +186,22 @@ public class PollMessageListener extends CustomMessageCreateListener {
 		}
 		
 		//Creates a Poll on the channel using the createPoll method
-		else if(event.getMessageContent().equals("!createPoll")) {
+		else if(event.getMessageContent().startsWith("!createPoll")) {
 			getPolls(event.getChannel().getId());
 			createPoll(event);
 		}
 		
+	}
+	
+	private void saveJson() {
+		try {
+			PrintWriter pw = new PrintWriter(new FileWriter("src/main/java/org/jointheleague/modules/polls.json", false));
+			pw.print(jArr.toString());
+			pw.close();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 	
 	
