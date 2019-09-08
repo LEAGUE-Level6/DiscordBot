298ec2a2c631b764d3138572e41948f8daaf9ce4
sohanagarkar <33811100+FieryXY@users.noreply.github.com>
2019-09-08 12:33:40 -0700

    Json Rework, Poll Information Rework

--- src/main/java/org/jointheleague/modules/PollMessageListener.java
+++ src/main/java/org/jointheleague/modules/PollMessageListener.java
@@ -73,12 +73,11 @@ public class PollMessageListener extends CustomMessageCreateListener {
 			String[] parameters = fullMessage.substring(commandLength).trim().split(" ");
 			
 			
-			for(int i = 0; i < parameters.length; i++) {
+			/*for(int i = 0; i < parameters.length; i++) {
 				System.out.println(parameters[i]);
-			}
+			}*/
 			if(parameters.length >= 3) {
 				try {
-					//TODO add parameters into poll json
 					
 					
 					JsonObject newPoll = new JsonObject();
@@ -91,6 +90,7 @@ public class PollMessageListener extends CustomMessageCreateListener {
 						for(int j = 1; j < parameters.length; j++) {
 							if(parameters[j].contains("\"")) {
 								otherQuote = j;
+								break;
 							}
 						}
 						if(otherQuote == -1) {
@@ -105,16 +105,30 @@ public class PollMessageListener extends CustomMessageCreateListener {
 							}
 						}
 						
-						finalName = finalName.replaceFirst("\"", "");
+						finalName = finalName.replaceAll("\"", "");
 						
 						name = finalName;
 						nextParameter = otherQuote+1;
 						
 					}
+					else {nextParameter++;}
 					newPoll.addProperty("name", name);
 					
+					//TODO convert JsonProperty to options JsonArray with name and count
 					String options = parameters[nextParameter];
-					newPoll.addProperty("options", options);
+					String[] optionArr = options.split(",");
+					if(optionArr.length <= 1) {
+						event.getChannel().sendMessage("Must have at least 2 options in a poll");
+						return;
+					}
+					JsonArray optionJsonArr = new JsonArray();
+					for(int j = 0; j < optionArr.length; j++) {
+						JsonObject tempOption = new JsonObject();
+						tempOption.addProperty("optionName", optionArr[j]);
+						tempOption.addProperty("optionCount", "0");
+						optionJsonArr.add(tempOption);
+					}
+					newPoll.add("options", optionJsonArr);
 					nextParameter++;
 					
 					String description = parameters[nextParameter];
@@ -123,6 +137,7 @@ public class PollMessageListener extends CustomMessageCreateListener {
 						for(int j = nextParameter+1; j < parameters.length; j++) {
 							if(parameters[j].contains("\"")) {
 								otherQuote = j;
+								break;
 							}
 						}
 						if(otherQuote == -1) {
@@ -130,20 +145,50 @@ public class PollMessageListener extends CustomMessageCreateListener {
 							return;
 						}
 						String finalName = "";
-						for(int j = 0; j <= otherQuote; j++) {
+						for(int j = nextParameter; j <= otherQuote; j++) {
 							finalName += parameters[j];
 							if(j != otherQuote) {
 								finalName += " ";
 							}
 						}
 						
-						finalName = finalName.replaceFirst("\"", "");
+						finalName = finalName.replaceAll("\"", "");
 						
 						description = finalName;
 						nextParameter = otherQuote+1;
 					}
 					
 					newPoll.addProperty("description", description);
+					
+					//TODO convert to JsonArray of participants (empty)
+					JsonArray participants = new JsonArray();
+					newPoll.add("participants", participants);
+					
+					/* 	if(optionArr.length > 1) {
+						String responseString = "";
+						for(int k = 0; k < optionArr.length; k++) {
+							responseString += "0";
+							if(k != optionArr.length-1) {
+								responseString += ",";
+							}
+						}
+						newPoll.addProperty("responses", responseString);
+					}
+					else {
+						event.getChannel().sendMessage("There must be at least two options in the poll");
+						return;
+					}*/
+					
+					newPoll.addProperty("owner", String.valueOf(event.getMessageAuthor().asUser().get().getMentionTag()));
+					JsonArray allPollsForThisChannel = (JsonArray) channelPolls.get("cPolls");
+					allPollsForThisChannel.forEach(pol -> {
+						JsonObject polObj = (JsonObject) pol;
+						if(polObj.get("name").getAsString().equals(newPoll.get("name").getAsString())) {
+							event.getChannel().sendMessage("Name Taken");
+							return;
+						}
+					});
+					channelPolls.get("cPolls").getAsJsonArray().add(newPoll);
 					
 					
 					event.getChannel().sendMessage(new EmbedBuilder()
@@ -153,24 +198,161 @@ public class PollMessageListener extends CustomMessageCreateListener {
 							.addField("Description", description)
 							.addField("Options", options)
 							);
+					saveJson();
 				}
 				catch(IndexOutOfBoundsException e) {
-					event.getChannel().sendMessage(new EmbedBuilder().setTitle("Invalid Parameters (IndexOutOfBoundsException)").setDescription("!createPoll \"<Poll Name>\" <Option1,Option2,Option3,...> \"<Description>\"").setColor(Color.RED));
+					event.getChannel().sendMessage(new EmbedBuilder().setTitle("Invalid Parameters (IndexOutOfBoundsException)").setDescription("!createPoll \"<Poll Name>\" <Option1,Option2,Option3,...> \"<Description>\" (Only use quotations around the desc and/or name is you want it to be multi-word)").setColor(Color.RED));
 				}
 				
 			}
 			else {
-				event.getChannel().sendMessage(new EmbedBuilder().setTitle("Invalid Parameters").setDescription("!createPoll \"<Poll Name>\" <Option1,Option2,Option3,...> \"<Description>\"").setColor(Color.RED));
+				event.getChannel().sendMessage(new EmbedBuilder().setTitle("Invalid Parameters").setDescription("!createPoll \"<Poll Name>\" <Option1,Option2,Option3,...> \"<Description>\" (Only use quotations around the desc and/or name is you want it to be multi-word)").setColor(Color.RED));
 			}
 		}
 		else {
-			event.getChannel().sendMessage(new EmbedBuilder().setTitle("Invalid Parameters").setDescription("!createPoll \"<Poll Name>\" <Option1,Option2,Option3,...> \"<Description>\"").setColor(Color.RED));
+			event.getChannel().sendMessage(new EmbedBuilder().setTitle("Invalid Parameters").setDescription("!createPoll \"<Poll Name>\" <Option1,Option2,Option3,...> \"<Description>\" (Only use quotations around the desc and/or name is you want it to be multi-word)").setColor(Color.RED));
 		}
 		
 	}
 	
+	private void displayPolls(MessageCreateEvent event) {
+		if(channelPolls != null) {
+			JsonArray allPC = (JsonArray) channelPolls.get("cPolls");
+			EmbedBuilder eb = new EmbedBuilder();
+			eb.setTitle("Polls for this Channel");
+			allPC.forEach(possiblePoll -> {
+				JsonObject possObj = (JsonObject) possiblePoll;
+				eb.addField(possObj.get("name").getAsString(), possObj.get("description").getAsString());
+			});
+			eb.setColor(Color.YELLOW);
+			event.getChannel().sendMessage(eb);
+		}
+	}
+	
+	private void infoForPoll(MessageCreateEvent event) {
+		String fullMessage = event.getMessageContent();
+		String command = "!pollInfo";
+		try {
+			String pollName = fullMessage.substring(command.length()).trim().replaceAll("\"", "");
+			if(pollName.trim().equals("")) {
+				event.getChannel().sendMessage(new EmbedBuilder()
+						.setTitle("Invalid Parameters")
+						.setDescription("!pollInfo \"<Poll Name>\"")
+						.setColor(Color.RED)
+						);
+				return;
+			}
+			//event.getChannel().sendMessage(pollName);
+			if(channelPolls != null) {
+				JsonArray pollInfoArr = channelPolls.get("cPolls").getAsJsonArray();
+				for(int j = 0; j < pollInfoArr.size(); j++) {
+					JsonObject tempPollInfoArr = pollInfoArr.get(j).getAsJsonObject();
+					if(tempPollInfoArr.get("name").getAsString().equals(pollName)) {
+						displayInformationPoll(tempPollInfoArr, event);
+						return;
+					}
+				}
+				event.getChannel().sendMessage(new EmbedBuilder().setTitle("Poll not Found").setColor(Color.RED));
+			}
+		}
+		catch(IndexOutOfBoundsException e) {
+			event.getChannel().sendMessage(new EmbedBuilder()
+					.setTitle("Invalid Parameters")
+					.setDescription("!pollInfo \"<Poll Name>\"")
+					.setColor(Color.RED)
+					);
+		}
+		
+		
+		
+	}
+	
+	private void displayInformationPoll(JsonObject pollObj, MessageCreateEvent event) {
+		System.out.println("test");
+		EmbedBuilder embedb = new EmbedBuilder();
+		embedb.setTitle("Poll Information");
+		embedb.addField("Name", pollObj.get("name").getAsString());
+		//TODO requires combining options array into String.
+		String optionStr = "";
+		JsonArray optionsJs = (JsonArray) pollObj.get("options");
+		for(int j = 0; j < optionsJs.size(); j++) {
+			JsonObject optObj =  (JsonObject) optionsJs.get(j);
+			optionStr += optObj.get("optionName").getAsString();
+			if(j != optionsJs.size()-1) {
+				optionStr += "\n";
+			}
+		}
+		embedb.addField("Options", optionStr);
+		embedb.addField("Description", pollObj.get("description").getAsString());
+		embedb.addField("Owner", pollObj.get("owner").getAsString());
+		event.getChannel().sendMessage(embedb);
+	}
+	
+	private void removePollParse(MessageCreateEvent event) {
+		String fullMessage = event.getMessageContent();
+		String command = "!removePoll" ;
+		try {
+			String pollName = fullMessage.substring(command.length()).trim().replaceAll("\"", "");
+			if(pollName.trim().equals("")) {
+				event.getChannel().sendMessage(new EmbedBuilder()
+						.setTitle("Invalid Parameters")
+						.setDescription("!removePoll \"<Poll Name>\"")
+						.setColor(Color.RED)
+						);
+				return;
+			}
+			//event.getChannel().sendMessage(pollName);
+			if(channelPolls != null) {
+				
+				JsonArray pollInfoArr = channelPolls.get("cPolls").getAsJsonArray();
+				for(int j = 0; j < pollInfoArr.size(); j++) {
+					JsonObject tempPollInfoArr = pollInfoArr.get(j).getAsJsonObject();
+					if(tempPollInfoArr.get("name").getAsString().equals(pollName)) {
+						if(tempPollInfoArr.get("owner").getAsString().equals((event.getMessageAuthor().asUser().get().getMentionTag()))) {
+							removePoll(event, tempPollInfoArr, j);
+						}
+						else {
+							event.getChannel().sendMessage(new EmbedBuilder().setTitle("Insufficient Permissions").setDescription("You are not the owner of the poll."));
+						}
+						
+						return;
+					}
+				}
+				event.getChannel().sendMessage(new EmbedBuilder().setTitle("Poll not Found").setColor(Color.RED));
+				
+			}
+		}
+		catch(IndexOutOfBoundsException e) {
+			event.getChannel().sendMessage(new EmbedBuilder()
+					.setTitle("Invalid Parameters")
+					.setDescription("!removePoll \"<Poll Name>\"")
+					.setColor(Color.RED)
+					);
+		}
+
+	}
+	
+	private void removePoll(MessageCreateEvent event, JsonObject toRemovePoll, int index) {
+		String removedName = toRemovePoll.get("name").getAsString();
+		JsonArray cPollsCopy = channelPolls.get("cPolls").getAsJsonArray();
+		cPollsCopy.remove(index);
+		channelPolls.remove("cPolls");
+		channelPolls.add("cPolls", cPollsCopy);
+		event.getChannel().sendMessage(new EmbedBuilder().setTitle("Poll Removed")
+				.setDescription("The following poll has been removed: " + removedName)
+				);
+		saveJson();
+	}
+	
+	
+	
 	@Override
 	public void handle(MessageCreateEvent event) throws APIException {
+		
+		if(channelPolls == null) {
+			getPolls(event.getChannel().getId());
+		}
+		
 		
 		//Will make an embed message of all the polls on a channel if there are any;
 		if(event.getMessageContent().equals("!polls")) {
@@ -181,7 +363,7 @@ public class PollMessageListener extends CustomMessageCreateListener {
 			}
 			else {
 				event.getChannel().sendMessage("Polls Received.");
-				//TODO Display Polls
+				displayPolls(event);
 			}
 		}
 		
@@ -189,6 +371,15 @@ public class PollMessageListener extends CustomMessageCreateListener {
 		else if(event.getMessageContent().startsWith("!createPoll")) {
 			getPolls(event.getChannel().getId());
 			createPoll(event);
+		}
+		else if(event.getMessageContent().startsWith("!pollInfo")) {
+			infoForPoll(event);
+		}
+		else if (event.getMessageContent().startsWith("!removePoll")) {
+			removePollParse(event);
+		}
+		else if(event.getMessageContent().startsWith("!votePoll")) {
+			
 		}
 		
 	}
