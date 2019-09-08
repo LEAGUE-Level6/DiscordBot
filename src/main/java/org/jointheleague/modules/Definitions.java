1992fa15b77694e1d4a743efa2d905e8a25a9a0f
League Student <league@iMac-11.attlocal.net>
2019-09-08 13:57:24 -0700

    Completed definitions

--- src/main/java/org/jointheleague/modules/Definitions.java
+++ src/main/java/org/jointheleague/modules/Definitions.java
@@ -1,5 +1,9 @@
 package org.jointheleague.modules;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.URL;
+
 import org.javacord.api.event.message.MessageCreateEvent;
 
 public class Definitions extends CustomMessageCreateListener{
@@ -12,7 +16,30 @@ public class Definitions extends CustomMessageCreateListener{
 		String a = event.getMessageContent();
 		if(a.startsWith("!define ")) {
 			String toBeFound = a.substring(8);
-			event.getChannel().sendMessage("https://www.dictionary.com/browse/"+toBeFound+"?s=t");
+			event.getChannel().sendMessage(Definition(toBeFound));
 		}
+	}
+	
+	public static String Definition(String s) {
+		String d = "Definition could not be found";
+		URL dictionarySite;
+		try {
+			dictionarySite = new URL("https://www.dictionary.com/browse/" + s + "?s=t");
+			BufferedReader in = new BufferedReader(new InputStreamReader(dictionarySite.openStream()));
+			String inputLine;
+			while ((inputLine = in.readLine()) != null) {
+				if (inputLine.length() > 34) {
+					if (inputLine.substring(0, 35).equals("  <meta name=\"description\" content=")) {
+						d= s + ": " + inputLine.substring(49 + s.length(), inputLine.length()-12);
+					}
+				}
+			}
+			in.close();
+
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			return "Definition could not be found";
+		}
+		return d;
 	}
 }
