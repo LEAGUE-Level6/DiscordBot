56eca2a1354805c1a1b9034d5a7e142501105654
sohanagarkar <33811100+FieryXY@users.noreply.github.com>
2019-10-12 12:30:44 -0700

    Created Morse Translation Method

--- src/main/java/org/jointheleague/modules/MorseTranslator.java
+++ src/main/java/org/jointheleague/modules/MorseTranslator.java
@@ -13,7 +13,7 @@ public class MorseTranslator extends CustomMessageCreateListener {
 	static final String TOGGLE_MORSE = "!morse-toggle";
 	static final String MANUAL_TRANSLATE = "!mrs";
 	
-	HashMap<String, String> englishMorse = new HashMap<String, String>();
+	HashMap<Character, String> englishMorse = new HashMap<String, String>();
 	ArrayList<String> toggledUsers = new ArrayList<String>();
 	
 	
@@ -28,6 +28,7 @@ public class MorseTranslator extends CustomMessageCreateListener {
 	
 	@Override
 	public void handle(MessageCreateEvent event) throws APIException {
+		//Toggles the Automatic Morse Translator
 		if(event.getMessageContent().startsWith(TOGGLE_MORSE)) {
 			if(toggledUsers.contains(findMentionTag(event))) {
 				toggledUsers.remove(findMentionTag(event));
@@ -38,60 +39,87 @@ public class MorseTranslator extends CustomMessageCreateListener {
 				event.getChannel().sendMessage("Automatic Morse Translation Toggled On");
 			}
 		}
-		if(event.getMessageContent().startsWith(MANUAL_TRANSLATE)) {
+		//Checks for the manual translation command
+		else if(event.getMessageContent().startsWith(MANUAL_TRANSLATE)) {
+			
+		}
+		//If the user has morse toggled, it will automatically edit his message to be in morse code
+		else if(toggledUsers.contains(event.getMessageAuthor().asUser().get().getMentionTag())) {
 			
 		}
 	}
 	
 	String translateMorse(String english) {
-		return null;
+		
+		String translated = "";
+		for(int i = 0; i < english.length(); i++) {
+			//Characters. Checks if the morse HashMap contains the character. If not, it just displays it in its original form.
+			if(englishMorse.containsKey(english.charAt(i))) {
+				translated += englishMorse.get(english.charAt(i));
+			}
+			else if(english.charAt(i) != ' '){
+				translated += english.charAt(i);
+			}
+			
+			//Whitespace. In Morse Code. Spaces are three spaces (used to identify end of word) and one space is used to identify the end of a character and the beginning of another
+			//morse character.
+			char nextCharacter = english.charAt(i+1);
+			if(nextCharacter == ' ') {
+				translated += "   ";
+			}
+			else {
+				translated += " ";
+			}
+		}
+		
+		return translated;
 	}
 	
 	void generateMorse() {
 		//Alphabet
-		englishMorse.put("a", ".-");
-		englishMorse.put("b", "-...");
-		englishMorse.put("c", "-.-.");
-		englishMorse.put("d", "-..");
-		englishMorse.put("e", ".");
-		englishMorse.put("f", "..-.");
-		englishMorse.put("g", "--.");
-		englishMorse.put("h", "....");
-		englishMorse.put("i", "..");
-		englishMorse.put("j", ".---");
-		englishMorse.put("k", "-.-");
-		englishMorse.put("l", ".-..");
-		englishMorse.put("m", "--");
-		englishMorse.put("n", "-.");
-		englishMorse.put("o", "---");
-		englishMorse.put("p", ".--.");
-		englishMorse.put("q", "--.-");
-		englishMorse.put("r", ".-.");
-		englishMorse.put("s", "...");
-		englishMorse.put("t", "-");
-		englishMorse.put("u", "..-");
-		englishMorse.put("v", "...-");
-		englishMorse.put("w", ".--");
-		englishMorse.put("x", "-..-");
-		englishMorse.put("y", "-.--");
-		englishMorse.put("z", "--..");
+		englishMorse.put('a', ".-");
+		englishMorse.put('b', "-...");
+		englishMorse.put('c', "-.-.");
+		englishMorse.put('d', "-..");
+		englishMorse.put('e', ".");
+		englishMorse.put('f', "..-.");
+		englishMorse.put('g', "--.");
+		englishMorse.put('h', "....");
+		englishMorse.put('i', "..");
+		englishMorse.put('j', ".---");
+		englishMorse.put('k', "-.-");
+		englishMorse.put('l', ".-..");
+		englishMorse.put('m', "--");
+		englishMorse.put('n', "-.");
+		englishMorse.put('o', "---");
+		englishMorse.put('p', ".--.");
+		englishMorse.put('q', "--.-");
+		englishMorse.put('r', ".-.");
+		englishMorse.put('s', "...");
+		englishMorse.put('t', "-");
+		englishMorse.put('u', "..-");
+		englishMorse.put('v', "...-");
+		englishMorse.put('w', ".--");
+		englishMorse.put('x', "-..-");
+		englishMorse.put('y', "-.--");
+		englishMorse.put('z', "--..");
 		
 		//Numerals
-		englishMorse.put("0", "-----");
-		englishMorse.put("1", ".----");
-		englishMorse.put("2", "..---");
-		englishMorse.put("3", "...--");
-		englishMorse.put("4", "....-");
-		englishMorse.put("5", ".....");
-		englishMorse.put("6", "-....");
-		englishMorse.put("7", "--...");
-		englishMorse.put("8", "---..");
-		englishMorse.put("9", "----.");
+		englishMorse.put('0', "-----");
+		englishMorse.put('1', ".----");
+		englishMorse.put('2', "..---");
+		englishMorse.put('3', "...--");
+		englishMorse.put('4', "....-");
+		englishMorse.put('5', ".....");
+		englishMorse.put('6', "-....");
+		englishMorse.put('7', "--...");
+		englishMorse.put('8', "---..");
+		englishMorse.put('9', "----.");
 		
 		//Punctuation
-		englishMorse.put(".", ".-.-.-");
-		englishMorse.put(",", "--..--");
-		englishMorse.put("?", "..--..");
+		englishMorse.put('.', ".-.-.-");
+		englishMorse.put(',', "--..--");
+		englishMorse.put('?', "..--..");
 	}
 
 }
