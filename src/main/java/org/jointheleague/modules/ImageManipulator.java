394acfc9ea9a5f78a1e4999bd0c7d413a0fb68ae
League Student <league@iMac-24.attlocal.net>
2020-01-04 17:18:46 -0800

    made some more progress
    almost done

--- src/main/java/org/jointheleague/modules/ImageManipulator.java
+++ src/main/java/org/jointheleague/modules/ImageManipulator.java
@@ -22,7 +22,7 @@ public class ImageManipulator extends CustomMessageCreateListener {
 	
 	URL url;
 boolean b = false;
-		
+		BufferedImage imagee;
 		public ImageManipulator(String channelName) {
 			super(channelName);
 			
@@ -32,9 +32,9 @@ boolean b = false;
 		public void handle(MessageCreateEvent event) {
 			if (event.getMessageContent().startsWith("!image")) {
 				event.getChannel().sendMessage("Please insert the image you want me to manipulate.");
-				if (event.getMessageContent() != null) {
+				String urll = event.getMessageContent().substring(6);
 				try {
-					url = new URL(event.getMessageContent());
+					url = new URL(urll);
 					event.getChannel().sendMessage("Step 1 Complete");
 				} catch (MalformedURLException e1) {
 					// TODO Auto-generated catch block
@@ -44,15 +44,28 @@ boolean b = false;
 				
 				
 				try {
-					Image image = ImageIO.read(url);
+					 Image image = ImageIO.read(url);
+					 imagee = (BufferedImage) image;
 					event.getChannel().sendMessage("Finalizing");
+					System.out.println("Image height " + imagee.getHeight());
+					
 				} catch (IOException e) {
 					// TODO Auto-generated catch block
 					e.printStackTrace();
 					event.getChannel().sendMessage("Not an image!");
 				}
-				}
-		event.getChannel().sendMessage("Is this the image you want me to use?");
+				
+		for (int i = 0; i < imagee.getHeight(); i++) {
+		for (int j = 0; j < imagee.getWidth(); j++) {	
+			int red =   (imagee.getRGB(i, j) >> 16) & 0xFF;
+			int green = (imagee.getRGB(i, j) >>  8) & 0xFF;
+			int blue =  (imagee.getRGB(i, j)      ) & 0xFF;
+				System.out.println("Red " + red + "Green " + green+ "Blue " + blue);
+		}
+		}
+		//event.getChannel().sendMessage("Is this the image you want me to use?");
+		event.getChannel().sendMessage(urll);
+		
 		
 			}
 		}
