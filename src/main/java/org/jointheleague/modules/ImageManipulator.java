f0e277ab640b14422e693964863fd6a5beba9d4b
League Student <league@iMac-21.attlocal.net>
2020-02-08 17:15:39 -0800

    asdf
    asdf

--- src/main/java/org/jointheleague/modules/ImageManipulator.java
+++ src/main/java/org/jointheleague/modules/ImageManipulator.java
@@ -1,7 +1,11 @@
 package org.jointheleague.modules;
 
 import java.awt.Color;
+import java.awt.Font;
+import java.awt.FontMetrics;
+import java.awt.Graphics2D;
 import java.awt.Image;
+import java.awt.RenderingHints;
 import java.awt.image.BufferedImage;
 import java.io.BufferedReader;
 import java.io.File;
@@ -24,6 +28,8 @@ public class ImageManipulator extends CustomMessageCreateListener {
 	URL url;
 	boolean b = false;
 	BufferedImage imagee;
+	StringBuilder sb;
+	
 
 	public ImageManipulator(String channelName) {
 		super(channelName);
@@ -55,31 +61,107 @@ public class ImageManipulator extends CustomMessageCreateListener {
 				e.printStackTrace();
 				event.getChannel().sendMessage("Not an image!");
 			}
+			 sb = new StringBuilder((imagee.getWidth() + 1) * imagee.getHeight());
 			checkChunks();
+			
 		}
+		File file = new File("downloaded.jpg");
+        try {
+			ImageIO.write(textToJPG(sb.toString()), "jpg", file);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+        event.getMessage().getChannel().sendMessage(file);
+
 
 	}
 	public void checkChunks() {
 		for (int z = 0; z < imagee.getHeight(); z += 3) {
 			for (int m = 0; m < imagee.getWidth(); m += 3) {
-				int chunkTotal = 0;
+				double chunkTotal = 0;
 
 				for (int i = z; i < z + 3; i++) {
 					for (int j = z; j < z + 3; j++) {
 						Color color = new Color(imagee.getRGB(i, j));
-						int red = color.getRed();
-						int green = color.getGreen();
-						int blue = color.getBlue();
-						//System.out.println("Red " + red + "Green " + green + "Blue " + blue);
+						double red = color.getRed() * 0.2989;
+						double green = color.getGreen() * 0.1140 ;
+						double blue = color.getBlue() * 0.5870;
+						System.out.println("Red " + red + "Green " + green + "Blue " + blue);
 						//System.out.println(" Z " + z + " M " + m + " I " + i + " J " + j);
-						int averageP = (red + green + blue) / 3;
+						double averageP = (red + green + blue) / 3;
 						chunkTotal += averageP;
 					}
 				}
-				int chunkAvg = chunkTotal / 9;
+				double chunkAvg = chunkTotal / 9;
+				sb.append(returnStrPos(chunkAvg));
 				System.out.println("Chunk average " + chunkAvg);
+				
 			}
 
 		}
 	}
+	private char returnStrPos(double g)//takes the grayscale value as parameter
+    {
+        final char str;
+
+        if (g >= 230.0) {
+            str = ' ';
+        } else if (g >= 200.0) {
+            str = '.';
+        } else if (g >= 180.0) {
+            str = '*';
+        } else if (g >= 160.0) {
+            str = ':';
+        } else if (g >= 130.0) {
+            str = 'o';
+        } else if (g >= 100.0) {
+            str = '&';
+        } else if (g >= 70.0) {
+            str = '8';
+        } else if (g >= 50.0) {
+            str = '#';
+        } else {
+            str = '@';
+        }
+        return str; // return the character
+
+    }
+	 public static BufferedImage textToJPG(String text) {
+	        /*
+	           Because font metrics is based on a graphics context, we need to create
+	           a small, temporary image so we can ascertain the width and height
+	           of the final image
+	         */
+	        BufferedImage img = new BufferedImage(1, 1, BufferedImage.TYPE_INT_RGB);
+	        Graphics2D g2d = img.createGraphics();
+	        Font font = new Font("Arial", Font.PLAIN, 48);
+	        g2d.setFont(font);
+	        FontMetrics fm = g2d.getFontMetrics();
+	        int width = fm.stringWidth(text);
+	        int height = fm.getHeight();
+	        g2d.dispose();
+
+	        img = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
+	        g2d = img.createGraphics();
+	        g2d.setRenderingHint(RenderingHints.KEY_ALPHA_INTERPOLATION, RenderingHints.VALUE_ALPHA_INTERPOLATION_QUALITY);
+	        g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+	        g2d.setRenderingHint(RenderingHints.KEY_COLOR_RENDERING, RenderingHints.VALUE_COLOR_RENDER_QUALITY);
+	        g2d.setRenderingHint(RenderingHints.KEY_DITHERING, RenderingHints.VALUE_DITHER_ENABLE);
+	        g2d.setRenderingHint(RenderingHints.KEY_FRACTIONALMETRICS, RenderingHints.VALUE_FRACTIONALMETRICS_ON);
+	        g2d.setRenderingHint(RenderingHints.KEY_INTERPOLATION, RenderingHints.VALUE_INTERPOLATION_BILINEAR);
+	        g2d.setRenderingHint(RenderingHints.KEY_RENDERING, RenderingHints.VALUE_RENDER_QUALITY);
+	        g2d.setRenderingHint(RenderingHints.KEY_STROKE_CONTROL, RenderingHints.VALUE_STROKE_PURE);
+	        g2d.setFont(font);
+	        
+	        fm = g2d.getFontMetrics();
+	        g2d.setColor(Color.WHITE);
+	        g2d.fillRect(0, 0, img.getWidth(), img.getHeight());
+	        g2d.setColor(Color.BLACK);
+	        g2d.drawString(text, 0, fm.getAscent());
+	        g2d.dispose();
+	        return img;
+
+	    	
+	    }  
 }
