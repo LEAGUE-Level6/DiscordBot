1deb6bbc7b88f9a513fd2eb0c16e33253f8c8dc5
githubdoesntwork <45023593+githubdoesntwork@users.noreply.github.com>
2020-02-10 09:47:52 -0800

    reeee

--- src/main/java/org/jointheleague/modules/Bot1Listener.java
+++ src/main/java/org/jointheleague/modules/Bot1Listener.java
@@ -21,14 +21,13 @@ public class Bot1Listener extends CustomMessageCreateListener {
 	public void handle(MessageCreateEvent event) throws APIException {
 		if (event.getMessageContent().contains("*bet") && event.getMessageAuthor().getId() == 622894941667983404L) {
 			// -------------------------------------------------------
-			String pass = "";
-			int cc = 0;
+			long cc = 0;
 			int luck = 0;
 			try {
 				BufferedReader r = new BufferedReader(new FileReader("src/main/resources/Untitled 1.txt"));
-				pass = r.readLine();
 				cc = Integer.parseInt(r.readLine());
 				luck = Integer.parseInt(r.readLine());
+				r.close();
 			} catch (FileNotFoundException e) {
 				e.printStackTrace();
 			} catch (IOException e) {
@@ -43,7 +42,7 @@ public class Bot1Listener extends CustomMessageCreateListener {
 
 			try {
 				String[] m = event.getMessageContent().split(" ");
-				int am = Integer.parseInt(m[1]);
+				long am = Integer.parseInt(m[1]);
 				if (am <= cc) {
 					int second = 0;
 					Random r = new Random();
@@ -66,11 +65,11 @@ public class Bot1Listener extends CustomMessageCreateListener {
 						}
 						event.getChannel().sendMessage(
 								"Darn! Your number was a smidge more than mine! Your multiplier was " + multi + ".");
-						cc += cc * multi * 0.01;
+						cc += am * multi * 0.01;
 						if (luck > 0) {
 							luck--;
 						}
-						fw.write(pass + "\n" + cc + "\n" + luck);
+						fw.write(cc + "\n" + luck);
 						fw.flush();
 
 					} else {
@@ -79,9 +78,13 @@ public class Bot1Listener extends CustomMessageCreateListener {
 						if (luck > 0) {
 							luck--;
 						}
-						fw.write(pass + "\n" + cc + "\n" + luck);
+						fw.write(cc + "\n" + luck);
 						fw.flush();
 					}
+				}else {
+					event.getChannel().sendMessage("Try again, but this time use ya brain");
+					fw.write(cc + "\n" + luck);
+					fw.flush();
 				}
 			} catch (Exception e) {
 				event.getChannel().sendMessage("Try again, but this time use ya brain");
@@ -90,15 +93,14 @@ public class Bot1Listener extends CustomMessageCreateListener {
 
 		// -----------------------------------------------------
 
-		if (event.getMessageContent().equals("*bal") && event.getMessageAuthor().getId() == 622894941667983404L) {
-			String pass = "";
-			int cc = 0;
+	if (event.getMessageContent().equals("*bal") && event.getMessageAuthor().getId() == 622894941667983404L) {
+			long cc = 0;
 			int luck = 0;
 			try {
 				BufferedReader r = new BufferedReader(new FileReader("src/main/resources/Untitled 1.txt"));
-				pass = r.readLine();
 				cc = Integer.parseInt(r.readLine());
 				luck = Integer.parseInt(r.readLine());
+				r.close();
 			} catch (FileNotFoundException e) {
 				e.printStackTrace();
 			} catch (IOException e) {
@@ -106,9 +108,53 @@ public class Bot1Listener extends CustomMessageCreateListener {
 			}
 			event.getChannel().sendMessage("Your current balance of CodingCoins is " + cc + ", and you have "+luck+" more lucky bets. lmaoooo git good");
 		}
+	
+	//----------------------------------------------------------
 		if (event.getMessageAuthor().getId() != 622894941667983404L
 				&& event.getMessageAuthor().getId() != 673595407011414016L) {
 			event.getChannel().sendMessage("Out, Normie!");
+		}
+		
+		//------------------------------------------------------
+		if (event.getMessageContent().equals("*shop") && event.getMessageAuthor().getId() == 622894941667983404L) {
+			event.getChannel().sendMessage("𝘜𝘴𝘦 𝘤𝘰𝘮𝘮𝘢𝘯𝘥 *𝘣𝘶𝘺 𝘵𝘰 𝘱𝘶𝘳𝘤𝘩𝘢𝘴𝘦 𝘢𝘯 𝘪𝘵𝘦𝘮.");
+			event.getChannel().sendMessage("There are the following items in the shop:");
+			event.getChannel().sendMessage("DICE 🎲: LUCK++ FOR 2-12 BETS");
+			event.getChannel().sendMessage("ID: dice");
+			event.getChannel().sendMessage("PRICE: 5000 CC");
+		}
+		
+		if (event.getMessageContent().contains("*buy") && event.getMessageAuthor().getId() == 622894941667983404L) {
+			long cc = 0;
+			int luck = 0;
+			try {
+				BufferedReader r = new BufferedReader(new FileReader("src/main/resources/Untitled 1.txt"));
+				cc = Integer.parseInt(r.readLine());
+				luck = Integer.parseInt(r.readLine());
+				r.close();
+			} catch (FileNotFoundException e) {
+				e.printStackTrace();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			String[] m = event.getMessageContent().split(" ");
+			String id = m[1];
+			if(id.equals("dice")&&cc>=5000) {
+				event.getChannel().sendMessage("You got a dice!");
+				Random r = new Random();
+				int l = r.nextInt(6)+1;
+				event.getChannel().sendMessage("You roll your new dice, hitting a "+l+".");
+				event.getChannel().sendMessage("Since you got a "+l+", you get a luck boost that lasts "+l*2+" bets.");
+				luck+=l*2;
+				cc-=5000;
+				try {
+					FileWriter fw = new FileWriter("src/main/resources/Untitled 1.txt");
+					fw.write(cc+"\n"+luck);
+					fw.flush();
+				}catch(IOException e) {
+					event.getChannel().sendMessage("Yikes, something mega wack");
+				}
+			}
 		}
 	}
 
