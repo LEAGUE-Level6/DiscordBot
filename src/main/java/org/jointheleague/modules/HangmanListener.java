e90db8644c6b18a65cbf19f88f4f7dcf96badb81
League Student <league@iMac-94.attlocal.net>
2020-02-16 15:19:53 -0800

    Created the final version
    This version makes you use !guess then a letter to guess the letter. You can also use !guessWord to guess the whole word.

--- src/main/java/org/jointheleague/modules/HangmanListener.java
+++ src/main/java/org/jointheleague/modules/HangmanListener.java
@@ -15,6 +15,7 @@ public class HangmanListener extends CustomMessageCreateListener{
 	
 	final String command = "!startHangman";
 	final String command2 = "!guess";
+	final String command3 = "!guessWord";
 	int lengthOfDictionary;
 	String word;
 	String displayed;
@@ -59,50 +60,102 @@ public class HangmanListener extends CustomMessageCreateListener{
 		
 	}
 	public void runGame(MessageCreateEvent event) throws InterruptedException {
-		if(word.contains(event.getMessageContent().charAt(0) + "") && !guessedLetters.contains(event.getMessageContent().charAt(0) + "")) {
-			guessedLetters += event.getMessageContent().charAt(0);
-			for(int i = 0; i < word.length(); i++) {
-				if(word.charAt(i) == event.getMessageContent().charAt(0)) {
-					if(i == 0) {
-						displayed = event.getMessageContent().charAt(0) + displayed.substring(2);
-					}
-					else {
-						displayed = displayed.substring(0, i * 3) + event.getMessageContent().charAt(0) + displayed.substring(i*3 + 2);
+		if(event.getMessageContent().contains(command2) && !event.getMessageContent().contains(command3)) {
+			char letter = event.getMessageContent().substring(event.getMessageContent().indexOf(command2) + command2.length()).trim().charAt(0);
+			if(word.contains(letter + "") && !guessedLetters.contains(letter + "")) {
+				guessedLetters += letter;
+				for(int i = 0; i < word.length(); i++) {
+					if(word.charAt(i) == letter) {
+						if(i == 0) {
+							displayed = letter + displayed.substring(1);
+						}
+						else {
+							displayed = displayed.substring(0, i * 2) + letter + displayed.substring(i*2 + 1);
+						}
 					}
 				}
-			}
-			event.getChannel().sendMessage(guessedLettersWrong);
-			event.getChannel().sendMessage(new File("src/main/resources/HangmanResources/deathStage " + onPic + ".png"));
-			Thread.sleep(10);
-			event.getChannel().sendMessage(displayed);
-		}
-		else if(guessedLetters.contains(event.getMessageContent().charAt(0) + "")) {
-			event.getChannel().sendMessage("You already guessed " + event.getMessageContent().charAt(0));
-		}
-		else {
-			if(onPic < 4) {
-				guessedLettersWrong += event.getMessageContent().charAt(0);
-				onPic ++;
 				event.getChannel().sendMessage(guessedLettersWrong);
 				event.getChannel().sendMessage(new File("src/main/resources/HangmanResources/deathStage " + onPic + ".png"));
 				Thread.sleep(10);
 				event.getChannel().sendMessage(displayed);
+				if(displayed.replaceAll(" ", "").equals(word)) {
+					event.getChannel().sendMessage("You Win");
+					gameStarted = false;
+					onPic = 0;
+					guessedLettersWrong = "";
+					guessedLetters = "";
+				}
+			}
+			else if(guessedLetters.contains(letter + "")) {
+				event.getChannel().sendMessage("You already guessed " + letter);
 			}
 			else {
-				onPic ++;
+				if(onPic < 5) {
+					guessedLettersWrong += letter + " ";
+					onPic ++;
+					event.getChannel().sendMessage(guessedLettersWrong);
+					event.getChannel().sendMessage(new File("src/main/resources/HangmanResources/deathStage " + onPic + ".png"));
+					Thread.sleep(10);
+					event.getChannel().sendMessage(displayed);
+				}
+				else {
+					onPic ++;
+					event.getChannel().sendMessage(guessedLettersWrong);
+					event.getChannel().sendMessage(new File("src/main/resources/HangmanResources/deathStage " + onPic + ".png"));
+					Thread.sleep(10);
+					event.getChannel().sendMessage("Game Over" + "\nThe correct word was " + word );
+					gameStarted = false;
+					onPic = 0;
+					guessedLettersWrong = "";
+					guessedLetters = "";
+					
+				}
+				
+			}
+			guessedLetters += letter;
+		}
+		else if(event.getMessageContent().contains(command3)) {
+			String wordG = event.getMessageContent().substring(event.getMessageContent().indexOf(command3) + command3.length()).trim();
+			if(word.equals(wordG)) {
+				displayed = "";
+				for(int i = 0; i < word.length(); i++) {
+					displayed += word.charAt(i) + " ";
+				}
 				event.getChannel().sendMessage(guessedLettersWrong);
 				event.getChannel().sendMessage(new File("src/main/resources/HangmanResources/deathStage " + onPic + ".png"));
 				Thread.sleep(10);
-				event.getChannel().sendMessage("Game Over" + "\nThe correct word was " + word );
+				event.getChannel().sendMessage(displayed);
+				event.getChannel().sendMessage("You Win");
 				gameStarted = false;
 				onPic = 0;
 				guessedLettersWrong = "";
 				guessedLetters = "";
 				
 			}
-			
+			else {
+				if(onPic < 5) {
+					onPic ++;
+					event.getChannel().sendMessage(guessedLettersWrong);
+					event.getChannel().sendMessage(new File("src/main/resources/HangmanResources/deathStage " + onPic + ".png"));
+					Thread.sleep(10);
+					event.getChannel().sendMessage(displayed);
+				}
+				else {
+					onPic ++;
+					event.getChannel().sendMessage(guessedLettersWrong);
+					event.getChannel().sendMessage(new File("src/main/resources/HangmanResources/deathStage " + onPic + ".png"));
+					Thread.sleep(10);
+					event.getChannel().sendMessage("Game Over" + "\nThe correct word was " + word );
+					gameStarted = false;
+					onPic = 0;
+					guessedLettersWrong = "";
+					guessedLetters = "";
+					
+				}
+				
+			}
 		}
-		guessedLetters += event.getMessageContent().charAt(0);
+		
 	}
 	
 	public void startGame(MessageCreateEvent event) throws InterruptedException {
@@ -129,7 +182,7 @@ public class HangmanListener extends CustomMessageCreateListener{
 				reader.close();
 				displayed = "";
 				for(int i = 0; i < word.length(); i++) {
-					displayed = displayed + "-- ";
+					displayed = displayed + "- ";
 				}
 				event.getChannel().sendMessage(new File("src/main/resources/HangmanResources/deathStage 0.png"));
 				Thread.sleep(10);
