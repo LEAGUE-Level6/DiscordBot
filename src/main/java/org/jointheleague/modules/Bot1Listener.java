115ef45b56ecf60712479275ca00a19339156d29
League Student <league@iMac-38.attlocal.net>
2020-02-23 11:32:23 -0800

    save :squirrel:

--- src/main/java/org/jointheleague/modules/Bot1Listener.java
+++ src/main/java/org/jointheleague/modules/Bot1Listener.java
@@ -5,6 +5,7 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Random;
 
 import org.javacord.api.event.message.MessageCreateEvent;
@@ -12,21 +13,31 @@ import org.javacord.api.event.message.MessageCreateEvent;
 import net.aksingh.owmjapis.api.APIException;
 
 public class Bot1Listener extends CustomMessageCreateListener {
-
+ArrayList<Person> arr = new ArrayList<Person>();
 	public Bot1Listener(String channelName) {
 		super(channelName);
 	}
 
 	@Override
 	public void handle(MessageCreateEvent event) throws APIException {
-		if (event.getMessageContent().contains("*bet") && event.getMessageAuthor().getId() == 622894941667983404L) {
+		if (event.getMessageContent().contains("*bet")) {
 			// -------------------------------------------------------
 			long cc = 0;
-			int luck = 0;
+			long luck = 0;
 			try {
 				BufferedReader r = new BufferedReader(new FileReader("src/main/resources/Untitled 1.txt"));
-				cc = Integer.parseInt(r.readLine());
-				luck = Integer.parseInt(r.readLine());
+				String str=r.readLine();
+				arr.clear();
+				while(str!=null) {
+					arr.add(new Person(Long.parseLong(str), Long.parseLong(r.readLine()), Long.parseLong(r.readLine())));
+					str=r.readLine();
+				}
+				for(Person p : arr) {
+					if(p.id==event.getMessageAuthor().getId()) {
+						cc = p.cc;
+						luck = p.luck;
+					}
+				}
 				r.close();
 			} catch (FileNotFoundException e) {
 				e.printStackTrace();
@@ -69,8 +80,10 @@ public class Bot1Listener extends CustomMessageCreateListener {
 						if (luck > 0) {
 							luck--;
 						}
-						fw.write(cc + "\n" + luck);
-						fw.flush();
+						for(Person p : arr) {
+							System.out.println("Hello");
+							fw.write(p.toString());
+						}
 
 					} else {
 						event.getChannel().sendMessage("Haha! I win uwu");
@@ -78,13 +91,16 @@ public class Bot1Listener extends CustomMessageCreateListener {
 						if (luck > 0) {
 							luck--;
 						}
-						fw.write(cc + "\n" + luck);
-						fw.flush();
+						for(Person p : arr) {
+							System.out.println("HELLo");
+							fw.write(p.toString());
+						}
 					}
 				}else {
 					event.getChannel().sendMessage("Try again, but this time use ya brain");
-					fw.write(cc + "\n" + luck);
-					fw.flush();
+					for(Person p : arr) {
+						fw.write(p.toString());
+					}
 				}
 			} catch (Exception e) {
 				event.getChannel().sendMessage("Try again, but this time use ya brain");
@@ -93,13 +109,23 @@ public class Bot1Listener extends CustomMessageCreateListener {
 
 		// -----------------------------------------------------
 
-	if (event.getMessageContent().equals("*bal") && event.getMessageAuthor().getId() == 622894941667983404L) {
+	if (event.getMessageContent().equals("*bal")) {
 			long cc = 0;
-			int luck = 0;
+			long luck = 0;
 			try {
 				BufferedReader r = new BufferedReader(new FileReader("src/main/resources/Untitled 1.txt"));
-				cc = Integer.parseInt(r.readLine());
-				luck = Integer.parseInt(r.readLine());
+				String str=r.readLine();
+				arr.clear();
+				while(str!=null) {
+					arr.add(new Person(Long.parseLong(str), Long.parseLong(r.readLine()), Long.parseLong(r.readLine())));
+					str=r.readLine();
+				}
+				for(Person p : arr) {
+					if(p.id==event.getMessageAuthor().getId()) {
+						cc = p.cc;
+						luck = p.luck;
+					}
+				}
 				r.close();
 			} catch (FileNotFoundException e) {
 				e.printStackTrace();
@@ -110,13 +136,8 @@ public class Bot1Listener extends CustomMessageCreateListener {
 		}
 	
 	//----------------------------------------------------------
-		if (event.getMessageAuthor().getId() != 622894941667983404L
-				&& event.getMessageAuthor().getId() != 673595407011414016L) {
-			event.getChannel().sendMessage("Out, Normie!");
-		}
-		
 		//------------------------------------------------------
-		if (event.getMessageContent().equals("*shop") && event.getMessageAuthor().getId() == 622894941667983404L) {
+		if (event.getMessageContent().equals("*shop")) {
 			event.getChannel().sendMessage("𝘜𝘴𝘦 𝘤𝘰𝘮𝘮𝘢𝘯𝘥 *𝘣𝘶𝘺 𝘵𝘰 𝘱𝘶𝘳𝘤𝘩𝘢𝘴𝘦 𝘢𝘯 𝘪𝘵𝘦𝘮.");
 			event.getChannel().sendMessage("There are the following items in the shop:");
 			event.getChannel().sendMessage("DICE 🎲: LUCK++ FOR 2-12 BETS");
@@ -124,13 +145,24 @@ public class Bot1Listener extends CustomMessageCreateListener {
 			event.getChannel().sendMessage("PRICE: 5000 CC");
 		}
 		
-		if (event.getMessageContent().contains("*buy") && event.getMessageAuthor().getId() == 622894941667983404L) {
+		if (event.getMessageContent().contains("*buy")) {
 			long cc = 0;
-			int luck = 0;
+			long luck = 0;
+			boolean found=false;
 			try {
 				BufferedReader r = new BufferedReader(new FileReader("src/main/resources/Untitled 1.txt"));
-				cc = Integer.parseInt(r.readLine());
-				luck = Integer.parseInt(r.readLine());
+				String str=r.readLine();
+				arr.clear();
+				while(str!=null) {
+					arr.add(new Person(Long.parseLong(str), Long.parseLong(r.readLine()), Long.parseLong(r.readLine())));
+					str=r.readLine();
+				}
+				for(Person p : arr) {
+					if(p.id==event.getMessageAuthor().getId()) {
+						cc = p.cc;
+						luck = p.luck;
+					}
+				}
 				r.close();
 			} catch (FileNotFoundException e) {
 				e.printStackTrace();
@@ -149,8 +181,9 @@ public class Bot1Listener extends CustomMessageCreateListener {
 				cc-=5000;
 				try {
 					FileWriter fw = new FileWriter("src/main/resources/Untitled 1.txt");
-					fw.write(cc+"\n"+luck);
-					fw.flush();
+					for(Person p : arr) {
+						fw.write(p.toString());
+					}
 				}catch(IOException e) {
 					event.getChannel().sendMessage("Yikes, something mega wack");
 				}
@@ -158,4 +191,22 @@ public class Bot1Listener extends CustomMessageCreateListener {
 		}
 	}
 
+}
+class Person {
+	long id;
+	long cc, luck;
+	public Person(long a, long b, long c) {
+		id =a;
+		cc=b;
+		luck=c;
+	}
+	public void set(long c) {
+		cc=c;
+	}
+	public void setL(long c) {
+		luck=c;
+	}
+	public String toString() {
+		return id+"\n"+cc+"\n"+luck+"\n";
+	}
 }
