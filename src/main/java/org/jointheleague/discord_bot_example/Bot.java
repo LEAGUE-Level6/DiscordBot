b115ea3467bf2f5cb382dea173a1237a72a5e7ef
League Student <league@iMac-11.attlocal.net>
2020-03-07 17:13:08 -0800

    Finished Vowel Feature

--- src/main/java/org/jointheleague/discord_bot_example/Bot.java
+++ src/main/java/org/jointheleague/discord_bot_example/Bot.java
@@ -2,19 +2,7 @@ package org.jointheleague.discord_bot_example;
 
 import org.javacord.api.DiscordApi; 
 import org.javacord.api.DiscordApiBuilder;
-import org.jointheleague.modules.CalculatorMessageListener;
-import org.jointheleague.modules.CasinoGameListener;
-import org.jointheleague.modules.RandomNumber;
-import org.jointheleague.modules.Weather;
-import org.jointheleague.modules.DadJokes;
-import org.jointheleague.modules.ClockMessageListener;
-import org.jointheleague.modules.ComicMessageListener;
-import org.jointheleague.modules.ElmoMessageListener;
-import org.jointheleague.modules.FactMessageListener;
-import org.jointheleague.modules.FlagMessageListener;
-import org.jointheleague.modules.NewPollMessageListener;
-import org.jointheleague.modules.FashionAdvisor;
-import org.jointheleague.modules.leetMessageListener;
+import org.jointheleague.modules.*;
 
 
 public class Bot  {
@@ -42,7 +30,7 @@ public class Bot  {
 		api.addMessageCreateListener(new ElmoMessageListener(channelName));
 		api.addMessageCreateListener(new FactMessageListener(channelName));
 		api.addMessageCreateListener(new CasinoGameListener(channelName));
-
+		api.addMessageCreateListener(new Ryland(channelName));
 		api.addMessageCreateListener(new leetMessageListener(channelName));
 
 		api.addMessageCreateListener(new FlagMessageListener(channelName));
