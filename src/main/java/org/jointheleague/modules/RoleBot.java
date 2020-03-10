48bc5cc791497d7e8ea715d89e487f122e7b95f2
League Student <league@iMac.attlocal.net>
2020-03-09 19:58:43 -0700

    Finished rolebot, messages when given incorrect inputs and deleting roles

--- src/main/java/org/jointheleague/modules/RoleBot.java
+++ src/main/java/org/jointheleague/modules/RoleBot.java
@@ -1,6 +1,7 @@
 package org.jointheleague.modules;
 
 import org.javacord.api.event.message.MessageCreateEvent;
+import org.javacord.api.listener.server.role.RoleDeleteListener;
 
 import java.awt.Color;
 import java.util.ArrayList;
@@ -10,6 +11,8 @@ import java.util.Map;
 import java.util.Optional;
 import java.util.concurrent.CompletableFuture;
 
+import org.javacord.api.entity.channel.Channel;
+import org.javacord.api.entity.channel.TextChannel;
 import org.javacord.api.entity.permission.Role;
 import org.javacord.api.entity.permission.RoleBuilder;
 import org.javacord.api.entity.server.Server;
@@ -41,63 +44,90 @@ public class RoleBot extends CustomMessageCreateListener {
 	}
 		
 	public void handle(MessageCreateEvent event) {
-		if (event.getMessageContent().startsWith("!role")) {
-			if (event.getMessageContent().equals("!role")) {
-				event.getChannel().sendMessage("This command can edit your roles: ");
-				event.getChannel().sendMessage("use **!role set [Role Name]** to set a role, and **!role remove [Role Name]** to remove a role");				
-				event.getChannel().sendMessage("The current editable roles are: ");
+		TextChannel channel = event.getChannel();
+		String input = event.getMessageContent();
+		User user = event.getMessageAuthor().asUser().get();
+		Server server = event.getServer().get();
+		StringBuilder stringBuilder = new StringBuilder();
+		if (input.startsWith("!role")) {
+			if (input.equals("!role")) {
+				stringBuilder.append("This command can edit your roles: \n");
+				stringBuilder.append("use **!role set [Role Name]** to set a role, and **!role remove [Role Name]** to remove a role\n");				
+				stringBuilder.append("The current editable roles are: \n");
+				stringBuilder.append("```\n");
 				for(Role r : server.getRoles()) {
 					if(r.getName().startsWith(ROLE_PREFIX)) {
-						event.getChannel().sendMessage(r.getName().replaceAll(ROLE_PREFIX, ""));
+						stringBuilder.append(r.getName().replaceAll(ROLE_PREFIX, "")+"\n");
 					}
 				}
+				stringBuilder.append("```");
+				channel.sendMessage(stringBuilder.toString());
 			}
 			
-			if (event.getMessageContent().startsWith("!role set")) {
-				String input = event.getMessageContent().replaceAll("!role set ", "");
-				addRole(event.getMessageAuthor().asUser(), event.getServer(), roles.get(input));
+			if (input.startsWith("!role set")) {
+				String argument = input.replaceAll("!role set ", "");
+				roleCheck(channel, argument, ("You have been given the " + argument + " role."));
+				addRole(user, server, roles.get(argument));
 			}
-			if (event.getMessageContent().startsWith("!role remove")) {
-				String input = event.getMessageContent().replaceAll("!role remove ", "");
-				removeRole(event.getMessageAuthor().asUser(), event.getServer(), roles.get(input));
+			if (input.startsWith("!role remove")) {
+				String argument = input.replaceAll("!role remove ", "");
+				roleCheck(channel, argument, ("You have removed the " + argument + " role."));
+				removeRole(user, server, roles.get(argument));
 			}
-			if (event.getMessageContent().startsWith("!role create")) {
-				if (event.getMessageAuthor().asUser().get().getId()==422983171471179790L) {
-					String input = event.getMessageContent().replaceAll("!role create ", "");
-					String[] str = input.split(" ");
-					try{
-						if (str.length == 4) {
-							
-						if (str[0].matches("^[a-zA-Z]+$")) {
-							rB.setName(ROLE_PREFIX+str[0]);
-						} else throw new Exception("Invalid color argument, typed " + str[0] +", expected String argument");
+			if (input.startsWith("!role create")) {
+				if (user.getId()==422983171471179790L) {
+					String argument = input.replaceAll("!role create ", "");
+					String[] str = argument.split(" ");
+					if (!roles.containsValue(roles.get(str[0]))) {
+						try{
+							if (str.length == 4) {
+								
+								if (str[0].matches("^[a-zA-Z_]+$")) {
+									rB.setName(ROLE_PREFIX+str[0]);
+								} else throw new Exception("Invalid color argument, typed " + str[0] +", expected String argument");
 						
-						rB.setColor(new Color(Integer.parseInt(str[1]), Integer.parseInt(str[2]), Integer.parseInt(str[3])));
-						CompletableFuture<Role> cf = rB.create();
-						roles.put(str[0], cf.get());
-						}
+								rB.setColor(new Color(Integer.parseInt(str[1]), Integer.parseInt(str[2]), Integer.parseInt(str[3])));
+								CompletableFuture<Role> cf = rB.create();
+								roles.put(str[0], cf.get());
+								channel.sendMessage("You have created a role with the name " + str[0] + ", and the rgb values of " + 
+										str[1] + " " + str[2] + " " + str[3] + " ");
+							}
 						else throw new Exception("Incorrect number of arguments: expected 4, recieved " + str.length);
-					} catch (Exception e) {
-						event.getChannel().sendMessage(e.getMessage());
+						} catch (Exception e) {
+							channel.sendMessage(e.getMessage());
+						}
 					}
+					else {channel.sendMessage("That role already exists!");}
 				}
+				else {channel.sendMessage("You don't have the authority to use this command :/");}
 			}
-			
+			if (input.startsWith("!role delete")) {
+				if (user.getId()==422983171471179790L) {
+					String argument = input.replaceAll("!role delete ", "");
+					roleCheck(channel, argument, ("You have deleted the " + argument + " role."));
+					roles.get(argument).delete();
+					roles.remove(argument);
+				}
+				else {channel.sendMessage("You don't have the authority to use this command :/");}
+			}
 		}
 	}
 	
-	private void removeRole(Optional<User> userop, Optional<Server> serverop, Role role) {
-		Server server = serverop.get();
-		User user = userop.get();
+	private void removeRole(User user, Server server, Role role) {
 		System.out.println(user);
 		System.out.println(server);
 		user.removeRole(role);
 		System.out.println(role);
 	}
+	
+	private void roleCheck(TextChannel channel, String argument, String message) {
+		if (roles.containsValue(roles.get(argument))) {
+			channel.sendMessage(message);
+		}
+		else channel.sendMessage("That role doesn't exist!");
+	}
 
-	private void addRole(Optional<User> userop, Optional<Server> serverop, Role role) {
-		Server server = serverop.get();
-		User user = userop.get();
+	private void addRole(User user, Server server, Role role) {
 		System.out.println(user);
 		System.out.println(server);
 		List<Role> roles = user.getRoles(server);
