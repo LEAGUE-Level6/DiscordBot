8d9200a99307cc3fa719daf9e80d2f90cbce3354
eliyahubowers <eliyahubowers@gmail.com>
2020-03-12 09:18:18 -0700

    stufs

--- src/main/java/org/jointheleague/modules/TicTacToe.java
+++ src/main/java/org/jointheleague/modules/TicTacToe.java
@@ -255,13 +255,13 @@ public class TicTacToe extends CustomMessageCreateListener {
 	}
 	
 	private void printBoard(MessageCreateEvent event) {
-		String output = "\n "
+		String output = "|\n"
 					  + "|		 " + board[0][0] + "    |    " + board[1][0] + "    |    " + board[2][0] + "\n"
 					  + "|		  " + "-	+	-	+	-"                                          + "\n"
 					  + "|		 " + board[0][1] + "    |    " + board[1][1] + "    |    " + board[2][1] + "\n"
 					  + "|		  " + "-	+	-	+	-"                                          + "\n"
 					  + "|		 " + board[0][2] + "    |    " + board[1][2] + "    |    " + board[2][2] + "\n"
-					  + "\n ";		
+					  + "|\n";		
 		event.getChannel().sendMessage(output);
 	}
 }
