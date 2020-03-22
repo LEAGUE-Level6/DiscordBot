2c071d98ca1a93a80b7c337f38be987ecc9f8fe0
Adi Gudi <agudi24@gmail.com>
2020-03-21 17:29:44 -0700

    done with fortune teller

--- src/main/java/org/jointheleague/modules/Test.java
+++ src/main/java/org/jointheleague/modules/Test.java
@@ -1,5 +1,22 @@
 package org.jointheleague.modules;
 
-public class Test {
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
+import org.javacord.api.event.message.MessageCreateEvent;
+
+public class Test extends CustomMessageCreateListener {
+
+	public Test(String channelName) {
+		super(channelName);
+	}
+
+	@Override
+	public void handle(MessageCreateEvent event) {
+		if (event.getMessageContent().startsWith("!8ball")) {
+			if (event.getMessageContent().equals("!8ball")) {
+				event.getChannel().sendMessage("Hello World");
+			}
+	}
+}
 }
