1c7e2c460473e895c2f257dcaf275872eacf06aa
audreychan <41796534+audreychan@users.noreply.github.com>
2020-03-22 11:36:16 -0700

    Added all code for Crazy Eights

--- src/main/java/org/jointheleague/modules/Card.java
+++ src/main/java/org/jointheleague/modules/Card.java
@@ -4,14 +4,33 @@ public class Card {
 
 	public int value;
 	public int suit;
+	public int declaredSuit;
 	
 	public Card(int v, int s) {
 		value = v;
 		suit = s;
+		declaredSuit = 0;
+	}
+	
+	public Card(int v, int s, int d) {
+		value = v;
+		suit = s;
+		declaredSuit = d;
 	}
 	
 	public String toString() {
 		String fin = "";
+		
+		switch(declaredSuit) {
+		case 1:
+			return "♤";
+		case 2:
+			return "♧";
+		case 3:
+			return "♡";
+		case 4:
+			return "♢";
+		}
 		
 		switch(value) {
 		case 1:
