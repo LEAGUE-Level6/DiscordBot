0e82548be59dd84c41f2e40d461f477d69c88f87
dhruvwa16 <dhruvwa16@gmail.com>
2020-03-21 17:28:47 -0700

    Code

--- src/main/java/org/jointheleague/modules/Dhruv.java
+++ src/main/java/org/jointheleague/modules/Dhruv.java
@@ -16,16 +16,23 @@ public class Dhruv extends CustomMessageCreateListener {
 
 	@Override
 	public void handle(MessageCreateEvent event) {
-		if (event.getMessageContent().contains(COMMAND)) {
-			
-			String cmd = event.getMessageContent().replaceAll(" ", "").replace("!Dhruv","");
-			System.out.println(cmd);
-							
+		if (event.getMessageContent().startsWith(COMMAND)) {
+			if(event.getMessageContent().equals(COMMAND)) {
+				event.getChannel().sendMessage("Type a sentence ");
+			}
+			 else {
+				String calc = event.getMessageContent().substring(7); 
+				sentenceCalculator(calc, event);
 				
-		
+			}
 			
 		}
-	}
-	
+		}
 
-}
+	public void sentenceCalculator(String calc, MessageCreateEvent event) {
+		// TODO Auto-generated method stub
+		String[] words = calc.split("//s+");
+		event.getChannel().sendMessage("This sentence has" + calc.length() + "characters");
+		event.getChannel().sendMessage("This sentence has" + words.length + "words");
+	}
+		}	
