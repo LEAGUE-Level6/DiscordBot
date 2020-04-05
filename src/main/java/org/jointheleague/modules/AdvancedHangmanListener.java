9066a356de612de9cc8cf6d2bdb3b387520dfd91
root <root@4520527c69f3>
2020-04-05 18:31:05 +0000

    phrase is empty in !uncover (broken)

--- src/main/java/org/jointheleague/modules/AdvancedHangmanListener.java
+++ src/main/java/org/jointheleague/modules/AdvancedHangmanListener.java
@@ -13,8 +13,9 @@ import net.aksingh.owmjapis.api.APIException;
 
 public class AdvancedHangmanListener extends CustomMessageCreateListener {
 	String helpCommand = "!help";
-	String guessCommand = "!guess";
 	String uncoverCommand = "!uncover";
+	String guessLetter = "!guessl";
+	String guessPhrase = "!guessp";
 	String startCommand = "!start";
 
 	public AdvancedHangmanListener(String channelName) {
@@ -31,7 +32,7 @@ public class AdvancedHangmanListener extends CustomMessageCreateListener {
 			event.getChannel().sendMessage(
 					"Welcome to Advanced Hangman! Here is how you play: \n 1) You will be presented with a random cliche phrase and given the same amount of boxes as the letters in each word.");
 			event.getChannel().sendMessage(
-					"2) Here are your commands: \n '!start'-starts a new game \n '!help'-shows all commands for reference \n '!uncover-use to uncover a letter; enter guesses in format (Word#,Letter#) \n '!guess'-use to make a guess");
+					"2) Here are your commands: \n '!start'-starts a new game \n '!help'-shows all commands for reference \n '!uncover-use to uncover a letter; enter guesses in format (Word#,Letter#) \n '!guessl'-guess a letter \n '!guessp' -guess a phrase");
 
 			event.getChannel().sendMessage("Here is your phrase:");
 			int r = new Random().nextInt();
@@ -53,18 +54,46 @@ public class AdvancedHangmanListener extends CustomMessageCreateListener {
 				
 				for (int i = 0; i < phrase.length(); i++) {
 					if (phrase.charAt(i) == ' ') {
-						begspaces = begspaces + " ";
+						begspaces = begspaces + "  ";
 					} else {
-						begspaces = begspaces + "_";
+						begspaces = begspaces + "_ ";
 					}
 				}
-				event.getChannel().sendMessage(begspaces);
-
+				event.getChannel().sendMessage("`"+ begspaces + "`");
+				event.getChannel().sendMessage("Your points: "+ pointTotal);
 			} catch (IOException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
 
+		}
+		if(event.getMessageContent().startsWith("!help")){
+			event.getChannel().sendMessage(
+					"Here are your commands: \n '!start'-starts a new game \n '!help'-shows all commands for reference \n '!uncover-use to uncover a letter; enter guesses in format (Word#,Letter#) \n '!guessl'-guess a letter \n '!guessp' -guess a phrase");
+		}
+		if(event.getMessageContent().startsWith("!uncover")) {
+			String uncoverletter = "";
+			String newbegspaces = begspaces;
+			int word = Integer.parseInt(event.getMessageContent().substring(9,10));
+			event.getChannel().sendMessage(word + "");
+			int letter = Integer.parseInt(event.getMessageContent().substring(11,12));
+			event.getChannel().sendMessage(letter + "");
+			System.out.println(phrase);
+			event.getChannel().sendMessage(phrase);
+			int currentword = 1;
+			for(int i = 0; i<phrase.length();i++) {
+				event.getChannel().sendMessage(phrase);
+				if(phrase.charAt(i)==' ') {
+					currentword++;
+					if(currentword == word) {
+						event.getChannel().sendMessage(currentword + "");
+						uncoverletter = phrase.substring(i+letter,i+letter+1);
+						event.getChannel().sendMessage(uncoverletter);
+						break;
+					}
+				}
+			}
+			event.getChannel().sendMessage(uncoverletter);
 		}
 	}
 
