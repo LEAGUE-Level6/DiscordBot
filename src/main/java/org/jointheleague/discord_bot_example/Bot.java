8d19e344414d840b6385d596380ed1ef70f614bb
githubdoesntwork <45023593+githubdoesntwork@users.noreply.github.com>
2020-04-05 11:30:49 -0700

    yeeee

--- src/main/java/org/jointheleague/discord_bot_example/Bot.java
+++ src/main/java/org/jointheleague/discord_bot_example/Bot.java
@@ -1,19 +1,20 @@
 package org.jointheleague.discord_bot_example;
 
-import org.javacord.api.DiscordApi; 
+import org.javacord.api.DiscordApi;
 import org.javacord.api.DiscordApiBuilder;
+import org.jointheleague.modules.Bot2;
 import org.jointheleague.modules.CalculatorMessageListener;
 import org.jointheleague.modules.CasinoGameListener;
-import org.jointheleague.modules.RandomNumber;
-import org.jointheleague.modules.Weather;
-import org.jointheleague.modules.DadJokes;
 import org.jointheleague.modules.ClockMessageListener;
 import org.jointheleague.modules.ComicMessageListener;
+import org.jointheleague.modules.DadJokes;
 import org.jointheleague.modules.ElmoMessageListener;
 import org.jointheleague.modules.FactMessageListener;
+import org.jointheleague.modules.FashionAdvisor;
 import org.jointheleague.modules.FlagMessageListener;
 import org.jointheleague.modules.NewPollMessageListener;
-import org.jointheleague.modules.FashionAdvisor;
+import org.jointheleague.modules.RandomNumber;
+import org.jointheleague.modules.Weather;
 import org.jointheleague.modules.leetMessageListener;
 
 
@@ -50,6 +51,7 @@ public class Bot  {
 		api.addMessageCreateListener(new Weather(channelName));
 		api.addMessageCreateListener(new FashionAdvisor(channelName));
 		api.addMessageCreateListener(new NewPollMessageListener(channelName));
+		api.addMessageCreateListener(new Bot2(channelName));
 	}
 
 }
