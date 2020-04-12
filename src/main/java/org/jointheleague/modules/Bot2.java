eb3360128b03714298bf873323bf85a779cca46d
githubdoesntwork <45023593+githubdoesntwork@users.noreply.github.com>
2020-04-12 11:29:41 -0700

    36-10

--- src/main/java/org/jointheleague/modules/Bot2.java
+++ src/main/java/org/jointheleague/modules/Bot2.java
@@ -7,35 +7,13 @@ public class Bot2 extends CustomMessageCreateListener{
 		super(channelName);
 	}
 	//  BOT'S ID: 691338351025848351
-	boolean in = false;
-	boolean waiting = false;
-	boolean f1=false, f2 = false;
-	long p1 = 0;
-	long p2 = 0;
-	boolean turn = true;
 	public void handle(MessageCreateEvent event) {
-		if(event.getMessageContent().equals("*tigtagtow")&&!in) {
-			event.getChannel().sendMessage("THE NEXT 2 PEOPLE TO TYPE ARE THE 2 PLAYERS");
-			waiting = true;
-		}else if(waiting){
-			if(f1==false) {
-				f1 = true;
-				p1 = event.getMessageAuthor().getId();
-			}else if(f2 == false && event.getMessageAuthor().getId()!=p1) {
-				f2 = true;
-				p2 = event.getMessageAuthor().getId();
-			}
-			waiting = false;
-			in = true;
-			boolean end = false;
-			event.getChannel().sendMessage("DA GAME BEGINS");
-			while(!end) {
-				if(turn)turn=false;
-				else turn = true;
-			}
-		}else if(event.getMessageAuthor().getId()!=691338351025848351L&&in&&!event.getMessageContent().equals("X")||!event.getMessageContent().equals("Y")) {
-			event.getChannel().sendMessage("Your foolish actions give me brain damage. There is a game in progress!");
+		if(event.getMessageAuthor().getId()!=691338351025848351L) {
+			event.getChannel().sendMessage("Simp, you blabber in base 36! Express your thoughts more clearly in Arabic.\nDon't you mean "+baseConversion(event.getMessageContent(), 36, 10));
 		}
+	}
+	public String baseConversion(String number, int sBase, int dBase) {
+		return Integer.toString(Integer.parseInt(number, sBase), dBase).toUpperCase();
 	}
 }
  
