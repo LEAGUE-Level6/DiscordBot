0642e9d8166c1eaac448966ca5e86d5c1ea242a8
Samuel Sharp <samuelsharp4@gmail.com>
2020-04-12 17:25:57 -0700

    Remove obfuscated code

--- src/main/java/org/jointheleague/discord_bot_example/Launcher.java
+++ src/main/java/org/jointheleague/discord_bot_example/Launcher.java
@@ -4,17 +4,7 @@ import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.Map;
-
-import com.google.auth.oauth2.GoogleCredentials;
-import com.google.firebase.FirebaseApp;
-import com.google.firebase.FirebaseOptions;
 import com.google.api.core.ApiFuture;
-import com.google.cloud.firestore.DocumentReference;
-import com.google.cloud.firestore.Firestore;
-import com.google.cloud.firestore.FirestoreOptions;
-import com.google.cloud.firestore.QueryDocumentSnapshot;
-import com.google.cloud.firestore.QuerySnapshot;
-import com.google.cloud.firestore.WriteResult;
 import com.google.common.collect.ImmutableMap;
 import java.util.HashMap;
 import java.util.List;
@@ -28,19 +18,6 @@ import java.util.Map;
  */
 public class Launcher {
 	public static void main(String[] args) {
-		//FileInputStream serviceAccount;
-		//try {
-			//serviceAccount = new FileInputStream("src/main/java/org/jointheleague/modules/key.json");
-			//FirebaseOptions options = new FirebaseOptions.Builder()
-					  //.setCredentials(GoogleCredentials.fromStream(serviceAccount))
-					  //.setDatabaseUrl("https://leaguelinks-9cafa.firebaseio.com")
-					  //.build();
-					
-					//FirebaseApp.initializeApp(options);
-		//} catch (IOException e) {
-			// TODO Auto-generated catch block
-			//e.printStackTrace();
-		//}
 		
 		if (args.length == 0) {
 			args = new String[] { "default" };
