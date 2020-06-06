1b38d0564dee659a764ae59bd9b85ff53994ee0c
mjfre <mjfre@aol.com>
2020-06-06 14:02:17 -0700

    fixing merge conflicts and removing print statement from ComicMessageListener

--- src/main/java/org/jointheleague/discord_bot_example/Bot.java
+++ src/main/java/org/jointheleague/discord_bot_example/Bot.java
@@ -10,18 +10,21 @@ import org.jointheleague.modules.*;
  * @author keithgroves and https://tinystripz.com
  *
  */
-public class Bot {
 
+public class Bot  {
+	
 	// The string to show the custom :vomiting_robot: emoji
 	public static String emoji = "<:vomiting_robot:642414033290657803>";
 
 	private String token;
 	private String channelName;
 	DiscordApi api;
+	_HelpListener helpListener;
 
 	public Bot(String token, String channelName) {
 		this.token = token;
 		this.channelName = channelName;
+		helpListener = new _HelpListener(channelName);
 	}
 
 	public void connect(boolean printInvite) {
@@ -33,8 +36,17 @@ public class Bot {
 			System.out.println("You can invite the bot by using the following url: " + api.createBotInvite());
 		}
 
-		// Load all the bots
-		api.addMessageCreateListener(new RandomNumber(channelName));
+		api.getServerTextChannelsByName(channelName).forEach(e -> e.sendMessage("Bot Connected"));
+		
+		//add Listeners
+		
+		RandomNumber randomNumber = new RandomNumber(channelName);
+		api.addMessageCreateListener(randomNumber);
+		helpListener.addHelpEmbed(randomNumber.getHelpEmbed());
+		
+		
+		api.addMessageCreateListener(helpListener);
+		api.addMessageCreateListener(new MomBot(channelName));
 		api.addMessageCreateListener(new DadJokes(channelName));
 		api.addMessageCreateListener(new ClockMessageListener(channelName));
 		api.addMessageCreateListener(new CalculatorMessageListener(channelName));
@@ -42,10 +54,21 @@ public class Bot {
 		api.addMessageCreateListener(new ElmoMessageListener(channelName));
 		api.addMessageCreateListener(new FactMessageListener(channelName));
 		api.addMessageCreateListener(new CasinoGameListener(channelName));
+		api.addMessageCreateListener(new Ryland(channelName));
+		api.addMessageCreateListener(new RockPaperScissorsListener(channelName));
 		api.addMessageCreateListener(new leetMessageListener(channelName));
+		api.addMessageCreateListener(new ConnectFour(channelName));
 		api.addMessageCreateListener(new FlagMessageListener(channelName));
+		api.addMessageCreateListener(new PictureOf(channelName));
+		api.addMessageCreateListener(new GetPicture(channelName));
+		api.addMessageCreateListener(new CuteAnimal(channelName));
 		api.addMessageCreateListener(new Weather(channelName));
 		api.addMessageCreateListener(new FashionAdvisor(channelName));
+		api.addMessageCreateListener(new MinesweeperListener(channelName));
 		api.addMessageCreateListener(new NewPollMessageListener(channelName));
+		api.addMessageCreateListener(new ComplimentListener(channelName));
+		api.addMessageCreateListener(new FEHStatListener(channelName));
+		api.addMessageCreateListener(new CrazyEights(channelName));
+		api.addMessageCreateListener(new Blackjack(channelName));
 	}
 }
