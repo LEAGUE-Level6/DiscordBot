40be9bb579356ad83d9eb5021ff7b752e1bb12d9
shachee <shacheeparikh@gmail.com>
2020-06-11 18:31:42 -0700

    Fixed the reset part of game.

--- src/main/java/org/jointheleague/modules/HighLowListener.java
+++ src/main/java/org/jointheleague/modules/HighLowListener.java
@@ -6,6 +6,8 @@ public class HighLowListener extends CustomMessageCreateListener {
 	private String c1 = "im";
 	private String c2 = "i'm";
 	private String c3 = "i am";
+	String HIGH ="";
+	String LOW ="";
 
 	int high = 100;
 	int low = 0;
@@ -20,7 +22,10 @@ public class HighLowListener extends CustomMessageCreateListener {
 		boolean play = false;
 
 		if (a.equals("!highlowgame")) {
-			System.out.println("hi");
+			high = 100;
+		    low = 0;
+		    HIGH ="!higher";
+		    LOW = "!lower";
 			event.getChannel().sendMessage(
 					"Choose a number between 1 and 100. I am going to guess this number. Respond with !higher or !lower, or if I got it !correct (case sensitive)");
 			event.getChannel().sendMessage("Is your number 50?");
@@ -29,15 +34,17 @@ public class HighLowListener extends CustomMessageCreateListener {
 
 		int guess = (low + high) / 2;
 
-		if (a.equals("!higher")) {
+		if (a.equals(HIGH)) {
 			low = guess + 1;
 			guess = (low + high) / 2;
 			event.getChannel().sendMessage("Is your number " + guess);
-		} else if (a.equals("!lower")) {
+		} else if (a.equals(LOW)) {
 			high = guess - 1;
 			guess = (low + high) / 2;
 			event.getChannel().sendMessage("Is your number " + guess);
 		} else if (event.getMessageContent().equals("!correct")) {
+			HIGH ="";
+			LOW ="";
 			event.getChannel().sendMessage("Yay, I got it, your number was " + guess);
 			play = false;
 		}
