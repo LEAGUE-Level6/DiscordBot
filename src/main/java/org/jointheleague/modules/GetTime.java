674c4285475acc1ed4ca9edac62be199e831c4df
OtisL <42128680+OtisL@users.noreply.github.com>
2020-06-22 17:01:30 -0700

    Continued Time

--- src/main/java/org/jointheleague/modules/GetTime.java
+++ src/main/java/org/jointheleague/modules/GetTime.java
@@ -1,12 +1,19 @@
 package org.jointheleague.modules;
 
+import org.javacord.api.entity.message.MessageBuilder;
+import org.javacord.api.entity.message.embed.EmbedBuilder;
 import org.javacord.api.event.message.MessageCreateEvent;
 
 import net.aksingh.owmjapis.api.APIException;
+
+import java.awt.Color;
 import java.time.Clock;
+import java.time.DateTimeException;
 import java.time.Instant;
+import java.time.LocalTime;
 import java.time.ZoneId;
 import java.time.ZoneOffset;
+import java.time.format.DateTimeFormatter;
 
 public class GetTime extends CustomMessageCreateListener{
 	public static final String COMMAND = "!time";
@@ -18,7 +25,45 @@ public class GetTime extends CustomMessageCreateListener{
 	@Override
 	public void handle(MessageCreateEvent event) throws APIException {
 		// TODO Auto-generated method stub
-			Clock clock = Clock.system(ZoneOffset.ofHours(-2));
-			System.out.println(clock.instant());
-		};
+			String text = event.getMessageContent();
+			text = text.trim();
+			if(text.equals(COMMAND)) {
+				String time = LocalTime.now(ZoneId.of("America/Los_Angeles")).toString();
+				time = time.substring(time.indexOf('T')+1, time.indexOf('.'));
+				new MessageBuilder()
+				.setEmbed(new EmbedBuilder()
+						.setTitle("Local Time")
+						.setDescription(time)
+						.setFooter("You can get the time of anywhere by !time <offset from GMT>"))
+				.send(event.getChannel());
+			}else if(text.startsWith(COMMAND)) {
+				try {
+					text = text.substring(text.indexOf(" ")+1);
+					ZoneId id = ZoneId.ofOffset("GMT", ZoneOffset.of(text));
+					String time = LocalTime.now(id).toString();
+					time = time.substring(time.indexOf('T')+1, time.indexOf('.'));
+					new MessageBuilder()
+					.setEmbed(new EmbedBuilder()
+							.setTitle("Time in "+id.getId())
+							.setDescription(time)
+							.setFooter("You can try offestting by hours and minutes like +00:20 or -10:45"))
+					.send(event.getChannel());
+				}catch(DateTimeException e){
+					System.out.println(e.getMessage());
+					System.out.println(e.getLocalizedMessage());
+					new MessageBuilder()
+					.setEmbed(new EmbedBuilder()
+							.setTitle("Error: "+e.getMessage())
+							.setDescription('"'+e.getMessage()+'"')
+							.addField("", "Maybe you inputted the offset wrong? \n"
+									+ "You must have it in one of these formats, with + or - before it: \n"
+									+ "h, hh, hh:mm \n"
+									+ "**h:mm does not work**")
+							.setColor(Color.RED))
+					.send(event.getChannel());
+				}
+				
+			}
+			
+		}
 	}
