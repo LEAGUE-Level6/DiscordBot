33bfde066e9ec75a099287dca98ec15cfa1921bd
Coddyfish <treelord05@gmail.com>
2020-06-30 16:53:43 -0700

    dev branch bot.java

--- src/main/java/org/jointheleague/discord_bot_example/Bot.java
+++ src/main/java/org/jointheleague/discord_bot_example/Bot.java
@@ -1,40 +1,66 @@
 package org.jointheleague.discord_bot_example;
 
 import org.javacord.api.DiscordApi;
+
 import org.javacord.api.DiscordApiBuilder;
-import org.jointheleague.modules.CalculatorMessageListener;
-import org.jointheleague.modules.CasinoGameListener;
-import org.jointheleague.modules.ClockMessageListener;
-import org.jointheleague.modules.ComicMessageListener;
-import org.jointheleague.modules.DadJokes;
-import org.jointheleague.modules.Economy;
-import org.jointheleague.modules.ElmoMessageListener;
-import org.jointheleague.modules.FactMessageListener;
-import org.jointheleague.modules.FashionAdvisor;
-import org.jointheleague.modules.FlagMessageListener;
-import org.jointheleague.modules.MomBot;
-import org.jointheleague.modules.NewPollMessageListener;
-import org.jointheleague.modules.Weather;
-import org.jointheleague.modules.leetMessageListener;
+
+/**
+ * Launches all of the listeners for one channel.
+ * @author keithgroves and https://tinystripz.com
+ *
+ */
+import org.jointheleague.modules.*;
 
 public class Bot {
+
+	// The string to show the custom :vomiting_robot: emoji
+	public static String emoji = "<:vomiting_robot:642414033290657803>";
 
 	private String token;
 	private String channelName;
 	DiscordApi api;
+	_HelpListener helpListener;
 
 	public Bot(String token, String channelName) {
 		this.token = token;
 		this.channelName = channelName;
+		helpListener = new _HelpListener(channelName);
 	}
 
-	public void connect() {
+	public void connect(boolean printInvite) {
+		
 		api = new DiscordApiBuilder().setToken(token).login().join();
-		System.out.println("You can invite the bot by using the following url: " + api.createBotInvite());
-		api.getServerTextChannelsByName(channelName).forEach(e -> e.sendMessage("Bot Connected"));
 
-		// add Listeners
-		api.addMessageCreateListener(new Economy(channelName));
+		// Print the URL to invite the bot
+		if (printInvite) {
+			System.out.println("You can invite the bot by using the following url: " + api.createBotInvite());
+		}
+
+		api.getServerTextChannelsByName(channelName).forEach(e -> e.sendMessage("Bot Connected"));
+		
+		//add Listeners
+		RandomNumber randomNumber = new RandomNumber(channelName);
+		api.addMessageCreateListener(randomNumber);
+		helpListener.addHelpEmbed(randomNumber.getHelpEmbed());
+		
+		SetProfilePic setPFP = new SetProfilePic(channelName);
+		api.addMessageCreateListener(setPFP);
+		helpListener.addHelpEmbed(setPFP.getHelpEmbed());
+
+		ToGif toGif = new ToGif(channelName);
+		api.addMessageCreateListener(toGif);
+		helpListener.addHelpEmbed(toGif.getHelpEmbed());
+		
+		RandomCase randomCase = new RandomCase(channelName);
+		api.addMessageCreateListener(randomCase);
+		helpListener.addHelpEmbed(randomCase.getHelpEmbed());
+		
+		_ApiExampleListener apiExampleListener = new _ApiExampleListener(channelName);
+		api.addMessageCreateListener(apiExampleListener);
+		helpListener.addHelpEmbed(apiExampleListener.getHelpEmbed());
+		
+		//old way to add listeners 
+		api.addMessageCreateListener(helpListener);
 		api.addMessageCreateListener(new MomBot(channelName));
 		api.addMessageCreateListener(new DadJokes(channelName));
 		api.addMessageCreateListener(new ClockMessageListener(channelName));
@@ -43,11 +69,39 @@ public class Bot {
 		api.addMessageCreateListener(new ElmoMessageListener(channelName));
 		api.addMessageCreateListener(new FactMessageListener(channelName));
 		api.addMessageCreateListener(new CasinoGameListener(channelName));
+		api.addMessageCreateListener(new HighLowListener(channelName));
+		api.addMessageCreateListener(new Ryland(channelName));
+		api.addMessageCreateListener(new RockPaperScissorsListener(channelName));
 		api.addMessageCreateListener(new leetMessageListener(channelName));
+		api.addMessageCreateListener(new ConnectFour(channelName));
 		api.addMessageCreateListener(new FlagMessageListener(channelName));
+		api.addMessageCreateListener(new EightBall(channelName));
+		api.addMessageCreateListener(new Reddit(channelName));
+		api.addMessageCreateListener(new DeepFrier(channelName));
+		api.addMessageCreateListener(new PictureOf(channelName));
+		api.addMessageCreateListener(new GetPicture(channelName));
+		api.addMessageCreateListener(new CuteAnimal(channelName));
 		api.addMessageCreateListener(new Weather(channelName));
+		api.addMessageCreateListener(new Economy(channelName));O
 		api.addMessageCreateListener(new FashionAdvisor(channelName));
-		api.addMessageCreateListener(new NewPollMessageListener(channelName));
-	}
+		api.addMessageCreateListener(new LatexRender(channelName));
+		api.addMessageCreateListener(new MinesweeperListener(channelName));
+		api.addMessageCreateListener(new Bot1Listener(channelName));
+		api.addMessageCreateListener(new PingMessageListener(channelName));	
+		api.addMessageCreateListener(new CoinFlipMessageListener(channelName));
+		api.addMessageCreateListener(new PlayRPSMessageListener(channelName));
+		api.addMessageCreateListener(new KickMessageListener(channelName));
+		api.addMessageCreateListener(new AssignRoleMessageListener(channelName));
+		api.addMessageCreateListener(new NicknameListener(channelName));
+		api.addMessageCreateListener(new SolveQuadraticListener(channelName));
+		api.addMessageCreateListener(new RollDiceMessageListener(channelName));
+		api.addMessageCreateListener(new MorseTranslator(channelName));
+		api.addMessageCreateListener(new HangmanListener(channelName));
+		api.addMessageCreateListener(new BogoSorterListener(channelName));
+		api.addMessageCreateListener(new ComplimentListener(channelName));
+		api.addMessageCreateListener(new CrazyEights(channelName));
+		api.addMessageCreateListener(new Blackjack(channelName));
 
+
+	}
 }
