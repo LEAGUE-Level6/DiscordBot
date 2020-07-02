c9cc86f272aa4f571862e0b38d532d5e21e0bdee
alvincvms <48074117+alvincvms@users.noreply.github.com>
2020-07-01 19:55:21 -0700

    bug fixes, usable now

--- src/main/java/org/jointheleague/modules/ScreenCapture.java
+++ src/main/java/org/jointheleague/modules/ScreenCapture.java
@@ -5,9 +5,10 @@ import java.awt.Rectangle;
 import java.awt.Robot;
 import java.awt.Toolkit;
 import java.awt.image.BufferedImage;
-import javax.swing.ImageIcon;
+import java.io.File;
 
-import org.javacord.api.entity.Icon;
+import javax.imageio.ImageIO;
+
 import org.javacord.api.entity.message.MessageBuilder;
 import org.javacord.api.event.message.MessageCreateEvent;
 import org.jointheleague.modules.pojo.HelpEmbed;
@@ -18,20 +19,21 @@ public class ScreenCapture extends CustomMessageCreateListener {
 
 	public ScreenCapture(String channelName) {
 		super(channelName);
-		helpEmbed = new HelpEmbed(COMMAND, "Takes a screenshot and sends it using the bot. The captured can be delayed by a specified value in milliseconds (e.g. !screenshot 5000).");
+		helpEmbed = new HelpEmbed(COMMAND, "Takes a screenshot, saves it to desktop, and sends it using the bot. The command can be delayed by a specified value in milliseconds (e.g. !screenshot 5000).");
 	}
 
 	@Override
 	public void handle(MessageCreateEvent event){
 		if(event.getMessageContent().toLowerCase().trim().startsWith(COMMAND)) {
 			try {
-				if(event.getMessageContent().replaceAll(" ", "").replace(COMMAND, "") != "") {
+				if(!event.getMessageContent().replaceAll(" ", "").replace(COMMAND, "").equals("")) {
 					int delay = Integer.parseInt(event.getMessageContent().replaceAll(" ", "").replace(COMMAND, ""));
 					Thread.sleep(delay);
 				}
 				
-				new MessageBuilder().addAttachment((Icon) new ImageIcon(getScreenShot()));
-				
+				event.getChannel().sendMessage("Screenshot saved!");
+				MessageBuilder builder = new MessageBuilder().addAttachment(new File(saveScreenShot()));
+				builder.send(event.getChannel());
 			} catch(NumberFormatException nfe) {
 				event.getChannel().sendMessage("[!screenshot error] Please format the command correctly: !screenshot <delay in millisec>");
 			} catch (InterruptedException ie) {
@@ -42,9 +44,31 @@ public class ScreenCapture extends CustomMessageCreateListener {
 		}
 	}
 	
-	private BufferedImage getScreenShot() throws AWTException {
+	private String saveScreenShot() throws AWTException {
 		Rectangle screen = new Rectangle(Toolkit.getDefaultToolkit().getScreenSize());
-		return new Robot().createScreenCapture(screen);
+		BufferedImage screenshot = new Robot().createScreenCapture(screen);
+		String name = "Screenshot.jpg";
+		int num = 0;
+		File[] files = new File(System.getProperty("user.home") + "/Desktop").listFiles();
+		whileLoop:
+		while(true) {
+			for(int i = 0; i < files.length; i++) {
+				if(files[i].getName().equals(name)) {
+					num++;
+					name = "Screenshot (" + num + ").jpg";
+					break;
+				}
+				if(i == files.length - 1) {
+					break whileLoop;
+				}
+			}
+		}
+		File output = new File(System.getProperty("user.home") + "/Desktop/" + name);
+		
+		try {
+			ImageIO.write(screenshot, "jpg", output);
+		} catch(Exception e) { }
+		return output.getPath();
 	}
 	
 }
