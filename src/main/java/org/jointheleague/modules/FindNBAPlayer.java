3d2a086104fa1e717b812e450a826aeeba2725fe
Athena Hernández <64385980+athenachernandez@users.noreply.github.com>
2020-07-09 13:02:33 -0700

    Update FindNBAPlayer.java

--- src/main/java/org/jointheleague/modules/FindNBAPlayer.java
+++ src/main/java/org/jointheleague/modules/FindNBAPlayer.java
@@ -21,66 +21,75 @@ import com.google.gson.Gson;
 import net.aksingh.owmjapis.api.APIException;
 
 public class FindNBAPlayer extends CustomMessageCreateListener {
+	private static final String COMMAND = "!nbaplayer";
+	private final Gson gson = new Gson();
 
-	//API key is received through creating an account on the web site.
-			//API may not require a key, or may require the key as a header as in PictureOf.java
-			private static final String COMMAND = "!NBAPlayer";
-			private final Gson gson = new Gson();
-		  
-			public FindNBAPlayer(String channelName) {
-				super(channelName);
-				helpEmbed = new HelpEmbed(COMMAND, "Gets the team of selected NBA player (e.g. Kyrie Irving: Brooklyn Nets");
+	public FindNBAPlayer(String channelName) {
+		super(channelName);
+		helpEmbed = new HelpEmbed(COMMAND,
+				"!nbaplayer gives you the height, weight, position, "
+						+ "or team of a NBA player of your choosing. Exempli gratia: \"!nba height James Harden\" "
+						+ "would return \"James Harden is 6\'5\".\".");
+	}
 
-			}
+	@Override
+	public void handle(MessageCreateEvent event) {
+		if (event.getMessageContent().contains(COMMAND)) {
+			String msg = event.getMessageContent();
+			String name = "";
+			String[] phrase = msg.toLowerCase().split(" ");
+			if (phrase.length == 4) {
+				name = phrase[2] + " " + phrase[3];
+				msg = name.replaceAll(" ", "%20");
 
-			@Override
-			public void handle(MessageCreateEvent event) {
-				if(event.getMessageContent().contains(COMMAND)) {
+				String requestURL = "https://www.balldontlie.io/api/v1/players?search=" + msg;
 
-					//remove the command so we are only left with the search term
-					String msg = event.getMessageContent();
-
-					if (msg.equals(COMMAND)) {
-						event.getChannel().sendMessage("Please put a word after the command");
-					} else {
-						msg = msg.substring(msg.indexOf(' ')+1).replaceAll(" ", "%20");
-						String definition = getTeam(msg);
-						event.getChannel().sendMessage(definition);
-					}
-					
-				}
-			}
-			
-			public String getTeam(String name) {
-				
-				//create the request URL (can be found in the documentation)
-				String requestURL = "https://www.balldontlie.io/api/v1/players?search=" + name;
-				System.out.println(requestURL);
 				try {
-					
-					//the following code will probably be the same for your feature
 					URL url = new URL(requestURL);
 					HttpURLConnection con = (HttpURLConnection) url.openConnection();
 					con.setRequestMethod("GET");
 					JsonReader repoReader = Json.createReader(con.getInputStream());
-				    JsonObject userJSON = ((JsonObject) repoReader.read());
-				    con.disconnect();
+					JsonObject userJSON = ((JsonObject) repoReader.read());
+					con.disconnect();
 
-					//turn the json response into a java object
-					//you will need to create a java class that represents the response in org.jointheleague.modules.pojo
-					//you can use a tools like Postman and jsonschema2pojo.com to help with that
-				   System.out.println(userJSON.toString()); 
-				    //you can use postman to make the request and receive a response, then take that and put it right into jsonschema2pojo.com
-					//If using jsonschema2pojo.com, select Target Langue = java, Source Type = JSON, Annotation Style = Gson
-				    NBARequest request = gson.fromJson(userJSON.toString(), NBARequest.class);
-				    
-				    String teamName = request.getData().get(0).getTeam().getFullName();
-				    name = name.replaceAll("%20", " ");
-					//create the message
-					String message = name + " is on " + teamName;
+					name = name.replaceAll("%20", " ");
+					NBARequest request = gson.fromJson(userJSON.toString(), NBARequest.class);
+					String message = "";
+
+					String teamName = "";
+					Integer feet = 0;
+					Integer weight = 0;
+					String position = "";
+					Integer inches = 0;
+					if (phrase[1].equals("team")) {
+						teamName = request.getData().get(0).getTeam().getFullName();
+						message = name + " is/was on the " + teamName + ".";
+					} else if (phrase[1].equals("height")) {
+						feet = request.getData().get(0).getHeightFeet();
+						inches = request.getData().get(0).getHeightInches();
+						message = name + " is " + feet + "\'" + inches + "\".";
+					} else if (phrase[1].equals("weight")) {
+						weight = request.getData().get(0).getWeightPounds();
+						message = name + " weighs " + weight + " pounds.";
+					} else if (phrase[1].equals("position")) {
+						position = request.getData().get(0).getPosition();
+						message = name + " is a " + position + ".";
+					} else {
+						event.getChannel().sendMessage(phrase[1]
+								+ " is not a valid parameter. Valid parameters include team, height, weight, and position.");
+					}
 					
-					//send the message 
-					return message;
+					if (teamName == null) {
+						event.getChannel().sendMessage(name + " does not have a registered team in the database.");
+					} else if (feet == null) {
+						event.getChannel().sendMessage(name + " does not have a registered height in the database.");
+					} else if (weight == null) {
+						event.getChannel().sendMessage(name + " does not have a registered weight in the database.");
+					} else if (position == null) {
+						event.getChannel().sendMessage(name + " does not have a registered position in the database.");
+					} else {
+						event.getChannel().sendMessage(message);
+					}
 					
 
 				} catch (MalformedURLException e) {
@@ -90,10 +99,11 @@ public class FindNBAPlayer extends CustomMessageCreateListener {
 				} catch (IOException e) {
 					e.printStackTrace();
 				}
-
-				return "No info for " + name;
+			} else if (msg.equals(COMMAND)) {
+				event.getChannel().sendMessage("Please put a word after the command.");
+			} else {
+				event.getChannel().sendMessage("Invalid command was entered. Enter four parameters next time.");
 			}
-			
-			
-
+		}
+	}
 }
