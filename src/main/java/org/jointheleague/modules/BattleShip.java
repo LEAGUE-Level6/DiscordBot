c8aeb733d7f57ff7cedff8e3c44db889dba42124
Eshaan Kansagara <eshaankansagara@gmail.com>
2020-07-26 17:28:03 -0700

    working on battleship some more
    added grids for both players. included commands for instructions and starting the game

--- src/main/java/org/jointheleague/modules/BattleShip.java
+++ src/main/java/org/jointheleague/modules/BattleShip.java
@@ -5,9 +5,12 @@ import javax.swing.JOptionPane;
 import org.javacord.api.event.message.MessageCreateEvent;
 
 public class BattleShip extends CustomMessageCreateListener {
-	String[][] grid = new String[10][10];
+	String[][] player1 = new String[10][10];
+	String[][] player2 = new String[10][10];
 
-	private static final String startCommand = "!battleship";
+	private static final String gameCommand = "!battleship";
+	private static final String instructionCommand = "!battleship-instructions";
+	private static final String startCommand = "!start";
 
 	public BattleShip(String channelName) {
 		super(channelName);
@@ -16,16 +19,19 @@ public class BattleShip extends CustomMessageCreateListener {
 
 	@Override
 	public void handle(MessageCreateEvent event) {
+		player1 = setPlayerGrid(player1);
+		player2 = setPlayerGrid(player2);
 
 		// TODO Auto-generated method stub
+		if (event.getMessageContent().equals(gameCommand)) {
+			event.getChannel()
+					.sendMessage("Enter *!battleship-instructions* for how to play\nEnter *!start* to begin the game");
+		}
+		if (event.getMessageContent().equals(instructionCommand)) {
+			event.getChannel().sendMessage("*****HOW TO PLAY*****");
+		}
 		if (event.getMessageContent().equals(startCommand)) {
-			for (int row = 0; row < grid.length; row++) {
-				for (int col = 0; col < grid[0].length; col++) {
-					grid[row][col] = "🌊\t";
-				}
-			}
-			event.getChannel().sendMessage(display(grid));
-
+			event.getChannel().sendMessage(display(player1));
 		}
 
 	}
@@ -39,9 +45,18 @@ public class BattleShip extends CustomMessageCreateListener {
 			matrix += "\n";
 		}
 		matrix += "-----------------------------------------------------------------------------------\n";
-		matrix += "0\t\t\t1\t\t\t2\t\t\t3\t\t\t4\t\t\t5\t\t\t6\t\t\t7\t\t\t8\t\t\t9\n\n  ";
+		matrix += "0\t\t\t1\t\t\t2\t\t\t3\t\t\t4\t\t\t5\t\t\t6\t\t\t7\t\t\t8\t\t\t9\n\n ";
 
 		return matrix;
 	}
 
+	public static String[][] setPlayerGrid(String[][] playerGrid) {
+
+		for (int row = 0; row < playerGrid.length; row++) {
+			for (int col = 0; col < playerGrid[0].length; col++) {
+				playerGrid[row][col] = "🌊\t";
+			}
+		}
+		return playerGrid;
+	}
 }
