b6825e5db802fd8321bce3ac374d041509858149
ArnavPalkhiwala <palkhiwala.arnav@gmail.com>
2020-07-29 17:46:32 -0700

    Added a bunch of features inside of this one website related feature. It is finished now!

--- src/main/java/org/jointheleague/modules/WebTaker.java
+++ src/main/java/org/jointheleague/modules/WebTaker.java
@@ -10,9 +10,19 @@ public class WebTaker extends CustomMessageCreateListener {
 	public WebTaker(String channelName) {
 		super(channelName);
 		// TODO Auto-generated constructor stub
-		helpEmbed = new HelpEmbed("WebFinder", "");
+		helpEmbed = new HelpEmbed("!WebFinder", "After typing !WebFinder just type any word, and it will take you to the .com version of the website. Ex. !WebFinder espn will take you to espn.com");
 
-		helpEmbed = new HelpEmbed("NFLStats", "");
+		helpEmbed = new HelpEmbed("!NFLStats", "After !NFLStats type a player to get their stats (full name).");
+		
+		helpEmbed = new HelpEmbed("!StatePopulation", "After !StatePopulation type the abbreviation for the state to get its population.");
+		
+		helpEmbed = new HelpEmbed("!CountryPopulation", "After !CountryPopulation type a country to get its population.");
+		
+		helpEmbed = new HelpEmbed("!CountryInfo", "After !CountryInfo type the country to get info about it");
+		
+		helpEmbed = new HelpEmbed("!NBAStats", "After !NBAStats type a player's full name to get their stats.");
+		
+
 
 	}
 
@@ -36,5 +46,26 @@ public class WebTaker extends CustomMessageCreateListener {
 			}
 			event.getChannel().sendMessage("https://www.nfl.com/players/" + player + "/");
 		}
+		
+		else if (message.contains("!StatePopulation")) {
+			String state = message.replace("!StatePopulation ", "").replaceAll(" ", "");
+			event.getChannel().sendMessage("https://www.census.gov/quickfacts/" + state);
+			}
+		
+		else if (message.contains("!CountryPopulation")) {
+			String country = message.replace("!CountryPopulation ", "").replaceAll(" ", "");
+			event.getChannel().sendMessage("https://www.worldometers.info/world-population/" + country + "-population/");
+			}
+		
+		else if (message.contains("!CountryInfo")) {
+			String country = message.replace("!CountryInfo ", "").replaceAll(" ", "");
+			event.getChannel().sendMessage("https://www.britannica.com/place/" + country);
+			}
+		
+		else if (message.contains("!NBAStats")) {
+			String players = message.replace("!NBAStats ", "").replaceAll(" ", "/");
+			event.getChannel().sendMessage("https://www.nba.com/players/" + players);
+			}
+	
 	}
 }
