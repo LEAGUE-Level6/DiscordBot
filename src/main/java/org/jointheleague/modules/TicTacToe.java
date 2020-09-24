123b2aca5d1b35fb2d130e41ee1035ac96268b66
lucas-khattar <59715368+lucas-khattar@users.noreply.github.com>
2020-09-23 18:25:55 -0700

    ticTacToe 9/23
    game is functional, next time im gonna make it check if there is a 3 in a row

--- src/main/java/org/jointheleague/modules/TicTacToe.java
+++ src/main/java/org/jointheleague/modules/TicTacToe.java
@@ -10,60 +10,93 @@ public class TicTacToe extends CustomMessageCreateListener {
 	}
 
 	private static final String COMMAND = "!ttt";
-
-	public void handle(MessageCreateEvent event) throws APIException {
-		String[] tiles = new String[10];
-		String message;
-		char num;
-		int tile;
-		for (int i = 0; i < tiles.length; i++) {
-			tiles[i] = " ";
+	String[] tiles = new String[10];
+	boolean gameInProgress = false;
+	boolean turn = false; //false = p1, true = p2
+	
+	public void makeTiles() {
+		if(!gameInProgress) {
+			for (int i = 0; i < tiles.length; i++) {
+				tiles[i] = "   ";
+			}
 		}
+	}
+	
+	public boolean check3InARow(String[] list) {
+		return false;
+	}
+	
+	public void handle(MessageCreateEvent event) throws APIException {
+		String message; //gets the message part after !ttt
+		char num; //gets tile number in a char value
+		int tile; //tile entered to put mark
+		makeTiles(); //calls makeTiles to make new tiles
+		
 		if (event.getMessageContent().contains(COMMAND)) {
 			message = event.getMessageContent().substring(5, event.getMessageContent().length());
 			if (message.contains("start")) {
-				event.getChannel().sendMessage("__ 1 | 2 | 3 __");
-				event.getChannel().sendMessage("__4 | 5 | 6 __");
-				event.getChannel().sendMessage(" 7 | 8 | 9 ");
-				event.getChannel().sendMessage("Use ! ttt p1 (tile#) to place an 'X' on that tile");
-				event.getChannel().sendMessage("Use ! ttt p2 (tile#) to place an 'O' on that tile");
-			} else if (message.contains("p1") | message.contains("p2")) {
-				num = message.charAt(3);
-				tile = Integer.parseInt(num + "");
-				if (message.contains("p1")) {
-					for (int i = 0; i < tiles.length; i++) {
-						if (i == tile) {
-							System.out.println(tiles[i]);
-							if (tiles[i] != " ") {
-								event.getChannel().sendMessage("Sorry, that spot is already taken :(");
-							} else {
-								tiles[i] = "X";
-								event.getChannel()
-										.sendMessage("__ " + tiles[1] + " | " + tiles[2] + " | " + tiles[3] + " __");
-								event.getChannel()
-										.sendMessage("__ " + tiles[4] + " | " + tiles[5] + " | " + tiles[6] + " __");
-								event.getChannel()
-										.sendMessage("** **" + tiles[7] + " | " + tiles[8] + " | " + tiles[9] + "** **");
+				gameInProgress = true;
+				turn = false;
+				event.getChannel().sendMessage("** 1 | 2 | 3 **");
+				event.getChannel().sendMessage("**__     |     |     __**");
+				event.getChannel().sendMessage("** 4 | 5 | 6 **");
+				event.getChannel().sendMessage("**__     |     |     __**");
+				event.getChannel().sendMessage("** 7 | 8 | 9 **");
+				event.getChannel().sendMessage("**     |     |     **");
+				event.getChannel().sendMessage("Use **!.ttt p1** (tile#) to place an 'X' on that tile");
+			} else if (gameInProgress) {
+				if (message.contains("p1") | message.contains("p2")) {
+					num = message.charAt(3);
+					tile = Integer.parseInt(num + "");
+					if (message.contains("p1")&&!turn) {
+						for (int i = 0; i < tiles.length; i++) {
+							if (i == tile) {
+								System.out.println(tiles[i]);
+								if (tiles[i] != "   ") {
+									event.getChannel().sendMessage("Sorry, that spot is already taken");
+								} else {
+									tiles[i] = "X";
+									event.getChannel().sendMessage(
+											"** " + tiles[1] + " | " + tiles[2] + " | " + tiles[3] + " **");
+									event.getChannel().sendMessage("**__     |     |     __**");
+									event.getChannel().sendMessage(
+											"** " + tiles[4] + " | " + tiles[5] + " | " + tiles[6] + " **");
+									event.getChannel().sendMessage("**__     |     |     __**");
+									event.getChannel().sendMessage(
+											"** " + tiles[7] + " | " + tiles[8] + " | " + tiles[9] + " **");
+									event.getChannel().sendMessage("**     |     |     **");
+									event.getChannel().sendMessage("Use **!.ttt p2** (tile#) to place an 'O' on that tile");
+									turn = true;
+								}
 							}
 						}
-					}
-				} else if (message.contains("p2")) {
-					for (int i = 0; i < tiles.length; i++) {
-						if (i == tile) {
-							if (tiles[i] != " ") {
-								event.getChannel().sendMessage("Sorry, that spot is already taken :(");
-							} else {
-								tiles[i] = "O";
-								event.getChannel()
-										.sendMessage("__ " + tiles[1] + " | " + tiles[2] + " | " + tiles[3] + " __");
-								event.getChannel()
-										.sendMessage("__ " + tiles[4] + " | " + tiles[5] + " | " + tiles[6] + " __");
-								event.getChannel()
-										.sendMessage("** **" + tiles[7] + " | " + tiles[8] + " | " + tiles[9] + "** **");
+					} else if (message.contains("p2")&&turn) {
+						for (int i = 0; i < tiles.length; i++) {
+							if (i == tile) {
+								if (tiles[i] != "   ") {
+									event.getChannel().sendMessage("Sorry, that spot is already taken");
+								} else {
+									tiles[i] = "O";
+									event.getChannel().sendMessage(
+											"** " + tiles[1] + " | " + tiles[2] + " | " + tiles[3] + " **");
+									event.getChannel().sendMessage("**__     |     |     __**");
+									event.getChannel().sendMessage(
+											"** " + tiles[4] + " | " + tiles[5] + " | " + tiles[6] + " **");
+									event.getChannel().sendMessage("**__     |     |     __**");
+									event.getChannel().sendMessage(
+											"** " + tiles[7] + " | " + tiles[8] + " | " + tiles[9] + " **");
+									event.getChannel().sendMessage("**     |     |     **");
+									event.getChannel().sendMessage("Use **!.ttt p1** (tile#) to place an 'X' on that tile");
+									turn = false;
+								}
 							}
 						}
+					} else {
+						event.getChannel().sendMessage("Please wait for your opponent, it is not your turn");
 					}
 				}
+			} else {
+				event.getChannel().sendMessage("Enter a valid commant or start a new game using **!.ttt start**");
 			}
 		}
 	}
