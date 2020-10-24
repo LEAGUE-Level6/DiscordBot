cb96444eb139a8cad73dbecffb9591d8a08f0587
root <root@c118c6cef6bb>
2020-10-24 22:58:34 +0000

    wordsearchpuzzlegenerator

--- src/main/java/org/jointheleague/modules/WordSearchPuzzleGenerator.java
+++ src/main/java/org/jointheleague/modules/WordSearchPuzzleGenerator.java
@@ -9,7 +9,7 @@ public class WordSearchPuzzleGenerator extends CustomMessageCreateListener{
 	public static final String COMMAND = "!generate word search puzzle:";
 	public WordSearchPuzzleGenerator(String channelName) {
 		super(channelName);
-		helpEmbed = new HelpEmbed(COMMAND, "Creates a 10 x 10 random word search puzzle with the words you want");
+		helpEmbed = new HelpEmbed(COMMAND, "Creates a 10 x 10 random word search puzzle with the words you want. except the words need to be less than 11 letters long");
 		
 	}
 
@@ -19,7 +19,7 @@ public class WordSearchPuzzleGenerator extends CustomMessageCreateListener{
 		if(event.getMessageContent().contains(COMMAND)) {
 		String response = event.getMessageContent().replace("!generate word search puzzle: ", "");
 		System.out.println(response);
-		int now = 1;
+		int now = 0;
 		for(int i = 0; i < response.length(); i++) {
 			if(response.charAt(i)== ',') {
 				now++;
@@ -27,7 +27,18 @@ public class WordSearchPuzzleGenerator extends CustomMessageCreateListener{
 		}
 		System.out.println(now + " words");
 		String[] words = new String[now];
-		//int number = 0;
+		int o = -1;
+		for(int n = 0; n < response.length(); n++) {
+			if(response.charAt(n)==',') {
+				o++;
+				words[o] = response.substring(0, response.indexOf(','));
+				//response = response.substring(response.indexOf(',')+1, response.length());
+			}
+		}
+		for(int i = 0; i < words.length; i++) {
+			System.out.println(words[i]);
+		}
+		char[][] puzzle = new char[10][10];
 			}
 		}
 		
