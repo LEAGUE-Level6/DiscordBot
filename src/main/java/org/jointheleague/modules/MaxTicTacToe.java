0da36c56df4b8528b011a88fb70fba3cd671e880
MaxSpiro <49620236+MaxSpiro@users.noreply.github.com>
2020-11-01 13:40:46 -0800

    made some logic

--- src/main/java/org/jointheleague/modules/MaxTicTacToe.java
+++ src/main/java/org/jointheleague/modules/MaxTicTacToe.java
@@ -4,7 +4,8 @@ import org.javacord.api.event.message.MessageCreateEvent;
 
 import net.aksingh.owmjapis.api.APIException;
 
-public class MaxTicTacToe extends CustomMessageCreateListener{
+public class MaxTicTacToe extends CustomMessageCreateListener {
+	String[] board = { "-", "-", "-", "-", "-", "-", "-", "-", "-" };
 
 	public MaxTicTacToe(String channelName) {
 		super(channelName);
@@ -13,20 +14,44 @@ public class MaxTicTacToe extends CustomMessageCreateListener{
 
 	@Override
 	public void handle(MessageCreateEvent event) throws APIException {
-		boolean playing = false;
-		String message = event.getMessageContent();
-		String topHalf = "|------|------|------|\r\n" + 
-				"|          |         |          |\r\n" + 
-				"|------|------|------|";
-		String middle = "|          |         |          |";
-		String bottomHalf = "|------|------|------|\r\n" + 
-				"|          |         |          |\r\n" + 
-				"|------|------|------|";
-		if(message.contains("!playTicTacToe") && !playing) {
-			playing = true;
-			event.getChannel().sendMessage(middle);
+		
+	}
+
+	public boolean checkWinner(String letter) {
+		for (int a = 0; a < 8; a++) {
+			String line = null;
+			switch (a) {
+			case 0:
+				line = board[0] + board[1] + board[2];
+				break;
+			case 1:
+				line = board[3] + board[4] + board[5];
+				break;
+			case 2:
+				line = board[6] + board[7] + board[8];
+				break;
+			case 3:
+				line = board[0] + board[3] + board[6];
+				break;
+			case 4:
+				line = board[1] + board[4] + board[7];
+				break;
+			case 5:
+				line = board[2] + board[5] + board[8];
+				break;
+			case 6:
+				line = board[0] + board[4] + board[8];
+				break;
+			case 7:
+				line = board[2] + board[4] + board[6];
+				break;
+
+			}
+			if (line.equals("" + letter + letter + letter))
+				return true;
 
 		}
+		return false;
 	}
-	
+
 }
