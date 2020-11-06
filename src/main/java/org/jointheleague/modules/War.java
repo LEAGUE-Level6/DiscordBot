ea795c41b00fd6dfa2530e0c933b9b70789cc88f
edemarest <Lunacat7!>
2020-11-05 18:28:47 -0800

    11/5/20

--- src/main/java/org/jointheleague/modules/War.java
+++ src/main/java/org/jointheleague/modules/War.java
@@ -23,35 +23,45 @@ public class War extends CustomMessageCreateListener {
 		if(event.getMessageAuthor().getName().equals("Ella's Bot")) {
 		return;
 		}
-		gameStart = true;
+		int turns = 0;
+		gameStart = false;
 		String a = event.getMessageContent();
 		ArrayList<Integer> cards = new ArrayList<Integer>();
 		ArrayList<Integer> pD = new ArrayList<Integer>();
 		ArrayList<Integer> bD = new ArrayList<Integer>();
 		for(int i = 1; i <= 10; i++) {
 			for(int j = 0; j < 4; j++) {
-				cards.add(i);
-				
-		}
-		}
-		for(int i = 0; i < 20; i++) {
-			int index = new Random().nextInt(20);
+				cards.add(i);	
+			}
+		}	
+		for(int i = 0; i < cards.size()/2; i++) {
+			int index = new Random().nextInt(40-i);
 			pD.add(cards.get(index));
+			System.out.println("ADDING "+cards.get(index)+" to player deck");
 			cards.remove(index);
 		}
 		for(int i = 0; i < 20; i++) {
-			int index = new Random().nextInt(20);
+			int index = new Random().nextInt(20-i);
 			bD.add(cards.get(index));
 			cards.remove(index);
 		}
+
 		if(a.equals("!War")) {
-			int turns = 0;
+			gameStart = true;
 			event.getChannel().sendMessage("Game of War started. Shuffling the deck.");
 
 			
 		}
-		if(a.equals("flip") && gameStart) {
-			event.getChannel().sendMessage("```Your card:   "+pD.get(1)+"\nBot card:   "+bD.get(3)+"```");
+		if(a.equals("flip")) {
+			System.out.println("FLIP RESISTERED");
+			event.getChannel().sendMessage("```Your card:   "+pD.get(turns)+"\nBot card:   "+bD.get(turns)+"```");
+			if(pD.get(turns)>(pD.get(turns))) {
+				event.getChannel().sendMessage("```Your card was greater than the Bot's! You win the round! Points: 1```");
+			}
+			else {
+				event.getChannel().sendMessage("```Your card was less than the Bot's. You lose the round! Points: 0```");
+			}
+			turns++;
 		}
 	}
 }
