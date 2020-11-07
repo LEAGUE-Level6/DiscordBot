7cff35573d545c1e9a134a308e07110fb07c9d18
emma-l-myoosik <lilypaw777@gmail.com>
2020-11-06 16:13:05 -0800

    created new feature

---
+++ src/main/java/org/jointheleague/modules/ConcertTicket.java
@@ -0,0 +1,21 @@
+package org.jointheleague.modules;
+
+import org.javacord.api.event.message.MessageCreateEvent;
+
+import net.aksingh.owmjapis.api.APIException;
+
+public class ConcertTicket extends CustomMessageCreateListener {
+	private static final String COMMAND = "!concerttickets";
+	
+	public ConcertTicket(String channelName) {
+		super(channelName);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void handle(MessageCreateEvent event) throws APIException {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
