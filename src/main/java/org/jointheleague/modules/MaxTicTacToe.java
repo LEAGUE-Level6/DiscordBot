ea36e973d381cc9858c7860756175dedd5c6dd6d
MaxSpiro <49620236+MaxSpiro@users.noreply.github.com>
2020-11-15 13:15:01 -0800

    copied from other source

--- src/main/java/org/jointheleague/modules/MaxTicTacToe.java
+++ src/main/java/org/jointheleague/modules/MaxTicTacToe.java
@@ -1,57 +1,63 @@
 package org.jointheleague.modules;
 
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Random;
+
 import org.javacord.api.event.message.MessageCreateEvent;
+import org.jointheleague.modules.pojo.HelpEmbed;
 
 import net.aksingh.owmjapis.api.APIException;
 
 public class MaxTicTacToe extends CustomMessageCreateListener {
-	String[] board = { "-", "-", "-", "-", "-", "-", "-", "-", "-" };
+	private char[][] board = new char[3][3];
+	private String turn;
+	boolean playing;
 
 	public MaxTicTacToe(String channelName) {
 		super(channelName);
-		// TODO Auto-generated constructor stub
+		helpEmbed = new HelpEmbed("!playTicTacToe",
+				"Play TicTacToe with the computer." + " Strict keyword usage follows: upperleft, top, upperright, "
+						+ "left, center, right, lowerleft, bottom, and lowerright.");
+// TODO Auto-generated constructor stub
 	}
 
 	@Override
 	public void handle(MessageCreateEvent event) throws APIException {
-		
-	}
+		if (event.getMessageContent().contains("!playTicTacToe")) {
+			resetBoard();
+			turn = "X";
+			playing = true;
+			while (true) {
+				event.getChannel()
+						.sendMessage("Enter row then column number (Ex. 1 2 for top middle or 3 3 for bottom right");
+				String[] input = event.getMessageContent().split(" ");
+				int row = Integer.parseInt(input[0]);
+				int col = Integer.parseInt(input[1]);
+				if (input.length == 2) {
+					if (row > 0 && row <= 3 && col > 0 && col <= 3) {
+						break;
+					}
 
-	public boolean checkWinner(String letter) {
-		for (int a = 0; a < 8; a++) {
-			String line = null;
-			switch (a) {
-			case 0:
-				line = board[0] + board[1] + board[2];
-				break;
-			case 1:
-				line = board[3] + board[4] + board[5];
-				break;
-			case 2:
-				line = board[6] + board[7] + board[8];
-				break;
-			case 3:
-				line = board[0] + board[3] + board[6];
-				break;
-			case 4:
-				line = board[1] + board[4] + board[7];
-				break;
-			case 5:
-				line = board[2] + board[5] + board[8];
-				break;
-			case 6:
-				line = board[0] + board[4] + board[8];
-				break;
-			case 7:
-				line = board[2] + board[4] + board[6];
-				break;
-
+				}
+				board[row][col] = 'x';
 			}
-			if (line.equals("" + letter + letter + letter))
-				return true;
-
 		}
-		return false;
 	}
 
+	public void printBoard() {
+	}
+
+	public void resetBoard() {
+		for (int i = 0; i < 3; i++) {
+			for (int j = 0; j < 3; j++) {
+				board[i][j] = '-';
+			}
+		}
+	}
+
+	public String checkWinner() {
+		return "";
+
+	}
 }
