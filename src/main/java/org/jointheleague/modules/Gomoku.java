618d4edaccee206435d5ab15201958564e4fabfb
harrisoncliew <41922141+harrisoncliew@users.noreply.github.com>
2020-12-07 16:17:41 -0800

    Gomoku (completed)

--- src/main/java/org/jointheleague/modules/Gomoku.java
+++ src/main/java/org/jointheleague/modules/Gomoku.java
@@ -28,171 +28,15 @@ public class Gomoku extends CustomMessageCreateListener {
 				"Starts a game of gomoku. !gomokuRules shows the rules. To play, put in a coordinate (e.g. !gm0,0) to place a marker there. Player 1's marker is ㊋. Player 2's marker is ㊌. To rename yourself, use !gmName (e.g. !gmName Player) during your turn.");
 	}
 
-//	boolean check(String checkinthisdirection, int y, int x, String player) {
-//		int counter = 0;
-//		if (checkinthisdirection.equals("left")) {
-//			for (int i = 0; i < gameboard.length; i++) {
-//				for (int j = 0; j < gameboard[i].length; j++) {
-//					if (gameboard[y][x - 1].equals(player)) {
-//						counter++;
-//					}
-//					if (gameboard[y][x - 2].equals(player)) {
-//						counter++;
-//					}
-//					if (gameboard[y][x - 3].equals(player)) {
-//						counter++;
-//					}
-//					if (counter == 3) {
-//						return true;
-//					}
-//				}
-//			}
-//		}
-//
-//		if (checkinthisdirection.equals("right")) {
-//			for (int i = 0; i < gameboard.length; i++) {
-//				for (int j = 0; j < gameboard[i].length; j++) {
-//					if (gameboard[y][x + 1].equals(player)) {
-//						counter++;
-//					}
-//					if (gameboard[y][x + 2].equals(player)) {
-//						counter++;
-//					}
-//					if (gameboard[y][x + 3].equals(player)) {
-//						counter++;
-//					}
-//					if (counter == 3) {
-//						return true;
-//					}
-//				}
-//			}
-//		}
-//
-//		if (checkinthisdirection.equals("up")) {
-//			for (int i = 0; i < gameboard.length; i++) {
-//				for (int j = 0; j < gameboard[i].length; j++) {
-//					if (gameboard[y - 1][x].equals(player)) {
-//						counter++;
-//					}
-//					if (gameboard[y - 2][x].equals(player)) {
-//						counter++;
-//					}
-//					if (gameboard[y - 3][x].equals(player)) {
-//						counter++;
-//					}
-//					if (counter == 3) {
-//						return true;
-//					}
-//				}
-//			}
-//		}
-//
-//		if (checkinthisdirection.equals("down")) {
-//			for (int i = 0; i < gameboard.length; i++) {
-//				for (int j = 0; j < gameboard[i].length; j++) {
-//					if (gameboard[y + 1][x].equals(player)) {
-//						counter++;
-//					}
-//					if (gameboard[y + 2][x].equals(player)) {
-//						counter++;
-//					}
-//					if (gameboard[y + 3][x].equals(player)) {
-//						counter++;
-//					}
-//					if (counter == 3) {
-//						return true;
-//					}
-//				}
-//			}
-//		}
-//
-//		if (checkinthisdirection.equals("down-left")) {
-//			for (int i = 0; i < gameboard.length; i++) {
-//				for (int j = 0; j < gameboard[i].length; j++) {
-//					if (gameboard[y + 1][x - 1].equals(player)) {
-//						counter++;
-//					}
-//					if (gameboard[y + 2][x - 2].equals(player)) {
-//						counter++;
-//					}
-//					if (gameboard[y + 3][x - 3].equals(player)) {
-//						counter++;
-//					}
-//					if (counter == 3) {
-//						return true;
-//					}
-//				}
-//			}
-//		}
-//
-//		if (checkinthisdirection.equals("down-right")) {
-//			for (int i = 0; i < gameboard.length; i++) {
-//				for (int j = 0; j < gameboard[i].length; j++) {
-//					if (gameboard[y + 1][x + 1].equals(player)) {
-//						counter++;
-//					}
-//					if (gameboard[y + 2][x + 2].equals(player)) {
-//						counter++;
-//					}
-//					if (gameboard[y + 3][x + 3].equals(player)) {
-//						counter++;
-//					}
-//					if (counter == 3) {
-//						return true;
-//					}
-//				}
-//			}
-//		}
-//
-//		if (checkinthisdirection.equals("up-left")) {
-//			for (int i = 0; i < gameboard.length; i++) {
-//				for (int j = 0; j < gameboard[i].length; j++) {
-//					if (gameboard[y - 1][x - 1].equals(player)) {
-//						counter++;
-//					}
-//					if (gameboard[y - 2][x - 2].equals(player)) {
-//						counter++;
-//					}
-//					if (gameboard[y - 3][x - 3].equals(player)) {
-//						counter++;
-//					}
-//					if (counter == 3) {
-//						return true;
-//					}
-//				}
-//			}
-//		}
-//
-//		if (checkinthisdirection.equals("up-right")) {
-//			for (int i = 0; i < gameboard.length; i++) {
-//				for (int j = 0; j < gameboard[i].length; j++) {
-//					if (gameboard[y - 1][x + 1].equals(player)) {
-//						counter++;
-//					}
-//					if (gameboard[y - 2][x + 2].equals(player)) {
-//						counter++;
-//					}
-//					if (gameboard[y - 3][x + 3].equals(player)) {
-//						counter++;
-//					}
-//					if (counter == 3) {
-//						return true;
-//					}
-//				}
-//			}
-//		}
-//
-//		return false;
-//
-//	}
+
 
 public String checkWin() {
 		
 		for(int i=0; i<gameboard.length; i++) {
 			for(int j=0; j<gameboard[i].length; j++) {
-				if(gameboard[i][j] != "") {
-					if(checkDown(i, j) || checkRight(i,j) || checkDiagonal(i,j)) {
-						 if(gameboard[i][j] == "") {
+				if(gameboard[i][j] != "▢") {
+					if(checkDown(i, j) || checkRight(i,j) || checkDiagonal(i,j) || checkOtherDiagonal(i,j)) {
+						 if(gameboard[i][j] == "㊋") {
 							 return "player1";
 						 }
 						 else {
@@ -202,8 +46,10 @@ public String checkWin() {
 				}
 			}
 		}
-		return null;
+		return "no-one";
 	}
+
+
 
 	public boolean checkDown(int x, int y) {
 		if (y <= gameboard[x].length - 5) {
@@ -269,6 +115,7 @@ public String checkWin() {
 			String command = event.getMessageContent().replaceAll(" ", "").replace("!gomoku", "");
 
 			if (inProgress == false) {
+				board = "";
 				for (int i = 0; i < gameboard.length; i++) {
 					for (int j = 0; j < gameboard[i].length; j++) {
 						gameboard[i][j] = "▢";
@@ -296,12 +143,12 @@ public String checkWin() {
 						"Players alternate turns placing a stone of their color on an empty square.\nThe winner is the first player to form an unbroken chain of five stones horizontally, vertically, or diagonally.\nThe origin of this board (0,0) is in the top left corner.");
 			}
 
-		} else if (event.getMessageContent().contains("!gm")) {
-			String command = event.getMessageContent().replace("!gm", "");
+		} else if (event.getMessageContent().contains("!gm") && inProgress) {
+			String command = event.getMessageContent().replace("!gm", "").replaceAll(" ", "");;
 
 			if (command.contains(",")) {
 
-				command.replaceAll(" ", "");
+				
 
 				String ycoor = command.substring(0, command.indexOf(','));
 				String xcoor = command.substring(command.indexOf(',') + 1);
@@ -315,27 +162,19 @@ public String checkWin() {
 				if (playerNum == 0) {
 					if (gameboard[xc][yc].equals("▢")) {
 						gameboard[xc][yc] = "㊋";
-						player1Down = true;
+						playerNum = 1;
 					} else {
 						event.getChannel().sendMessage("You can't put a marker there!");
 					}
 
-					if (player1Down) {
-						playerNum = 1;
-					}
-					player1Down = false;
+					
 				} else if (playerNum == 1) {
 					if (gameboard[xc][yc].equals("▢")) {
 						gameboard[xc][yc] = "㊌";
-						player2Down = true;
+						playerNum = 0;
 					} else {
 						event.getChannel().sendMessage("You can't put a marker there!");
 					}
-
-					if (player2Down) {
-						playerNum = 0;
-					}
-					player2Down = false;
 				}
 
 				board = "";
@@ -351,139 +190,34 @@ public String checkWin() {
 
 				}
 
-				int length = gameboard.length - 1;
-
-				for (int i = 0; i < gameboard.length; i++) {
-					for (int j = 0; j < gameboard[i].length; j++) {
-//						if (gameboard[i][j].equals("㊌")) {
-//							if (i + 4 < length) {
-//								if (check("up", i + 4, j, "㊌")) {
-//									event.getChannel().sendMessage(player2Name + " has won!");
-//								}
-//								if (i - 4 > -1) {
-//									if (check("down", i - 4, j, "㊌")) {
-//										event.getChannel().sendMessage(player2Name + " has won!");
-//									}
-//								}
-//								if (j + 4 < length) {
-//									if (check("left", i, j + 4, "㊌")) {
-//										event.getChannel().sendMessage(player2Name + " has won!");
-//									}
-//								}
-//								if (j - 4 > -1) {
-//									if (check("right", i, j - 4, "㊌")) {
-//										event.getChannel().sendMessage(player2Name + " has won!");
-//									}
-//								}
-//								if (j + 4 < length && i + 4 < length) {
-//									if (check("up-left", i + 4, j + 4, "㊌")) {
-//										event.getChannel().sendMessage(player2Name + " has won!");
-//									}
-//								}
-//								if (j + 4 < length && i - 4 > -1) {
-//									if (check("down-left", i - 4, j + 4, "㊌")) {
-//										event.getChannel().sendMessage(player2Name + " has won!");
-//									}
-//								}
-//								if (j - 4 > -1 && i + 4 < length) {
-//									if (check("up-right", i + 4, j - 4, "㊌")) {
-//										event.getChannel().sendMessage(player2Name + " has won!");
-//									}
-//								}
-//								if (j - 4 > -1 && i - 4 > -1) {
-//									if (check("down-right", i - 4, j - 4, "㊌")) {
-//										event.getChannel().sendMessage(player2Name + " has won!");
-//									}
-//								}
-//							}
-//						}
-//						if (gameboard[i][j].equals("㊋")) {
-//							if (i + 4 < length) {
-//								if (check("up", i + 4, j, "㊋")) {
-//									event.getChannel().sendMessage(player1Name + " has won!");
-//								}
-//								if (i - 4 > -1) {
-//									if (check("down", i - 4, j, "㊋")) {
-//										event.getChannel().sendMessage(player1Name + " has won!");
-//									}
-//								}
-//								if (j + 4 < length) {
-//									if (check("left", i, j + 4, "㊋")) {
-//										event.getChannel().sendMessage(player1Name + " has won!");
-//									}
-//								}
-//								if (j - 4 > -1) {
-//									if (check("right", i, j - 4, "㊋")) {
-//										event.getChannel().sendMessage(player1Name + " has won!");
-//									}
-//								}
-//								if (j + 4 < length && i + 4 < length) {
-//									if (check("up-left", i + 4, j + 4, "㊋")) {
-//										event.getChannel().sendMessage(player1Name + " has won!");
-//									}
-//								}
-//								if (j + 4 < length && i - 4 > -1) {
-//									if (check("down-left", i - 4, j + 4, "㊋")) {
-//										event.getChannel().sendMessage(player1Name + " has won!");
-//									}
-//								}
-//								if (j - 4 > -1 && i + 4 < length) {
-//									if (check("up-right", i + 4, j - 4, "㊋")) {
-//										event.getChannel().sendMessage(player1Name + " has won!");
-//									}
-//								}
-//								if (j - 4 > -1 && i - 4 > -1) {
-//									if (check("down-right", i - 4, j - 4, "㊋")) {
-//										event.getChannel().sendMessage(player1Name + " has won!");
-//									}
-//								}
-//							}
-//						}
-						if (gameboard[i][j].equals("㊋") || gameboard[i][j].equals("㊌")) {
-							if (checkDown(j, i)) {
-								if (gameboard[i][j].equals("㊋")) {
-									event.getChannel().sendMessage(player1Name + " has won!");
-								} else {
-									event.getChannel().sendMessage(player2Name + " has won!");
-								}
-							} else if (checkRight(j, i)) {
-								if (gameboard[i][j].equals("㊋")) {
-									event.getChannel().sendMessage(player1Name + " has won!");
-								} else {
-									event.getChannel().sendMessage(player2Name + " has won!");
-								}
-							} else if(checkDiagonal(j,i)) {
-								if (gameboard[i][j].equals("㊋")) {
-									event.getChannel().sendMessage(player1Name + " has won!");
-								} else {
-									event.getChannel().sendMessage(player2Name + " has won!");
-								}
-							} else if(checkOtherDiagonal(j,i)) {
-								if (gameboard[i][j].equals("㊋")) {
-									event.getChannel().sendMessage(player1Name + " has won!");
-								} else {
-									event.getChannel().sendMessage(player2Name + " has won!");
-								}
-							}
-						}
-
-					}
-				}
 
 				event.getChannel().sendMessage(board);
-				if (playerNum == 0) {
-					event.getChannel().sendMessage(player1Name + "'s turn");
-				} else {
-					event.getChannel().sendMessage(player2Name + "'s turn");
-				}
+						if(checkWin().equals("player1")) {
+							event.getChannel().sendMessage(player1Name+" has won!");
+							inProgress = false;
+							event.getChannel().sendMessage("Game Over!");
+						} else if(checkWin().equals("player2")) {
+							event.getChannel().sendMessage(player2Name+" has won!");
+							inProgress = false;
+							event.getChannel().sendMessage("Game Over!");
+						} else {
+							
+							if (playerNum == 0) {
+								event.getChannel().sendMessage(player1Name + "'s turn");
+							} else {
+								event.getChannel().sendMessage(player2Name + "'s turn");
+							}
+						}
+					
+				
 
 			} else if (command.contains("Name")) {
 				String name = command.substring(command.indexOf('e') + 2, command.length());
 				if (playerNum == 0) {
-					event.getChannel().sendMessage(player1Name + "'s new name is " + name);
+					event.getChannel().sendMessage("Player 1's new name is " + name);
 					player1Name = name;
 				} else {
-					event.getChannel().sendMessage(player2Name + "'s new name is " + name);
+					event.getChannel().sendMessage("Player 2's new name is " + name);
 					player2Name = name;
 				}
 			} else {
