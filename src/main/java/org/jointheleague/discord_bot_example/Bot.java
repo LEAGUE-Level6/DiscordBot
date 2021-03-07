3783ee9b021d4003d601f3fbea275114a6213b3f
gabreb <62906858+gabreb@users.noreply.github.com>
2021-03-06 17:56:52 -0800

    hu

--- src/main/java/org/jointheleague/discord_bot_example/Bot.java
+++ src/main/java/org/jointheleague/discord_bot_example/Bot.java
@@ -114,9 +114,9 @@ public class Bot  {
 		api.addMessageCreateListener(mttt);
 		helpListener.addHelpEmbed(mttt.getHelpEmbed());
     
-		Greeter g = new Greeter(channelName);
-		api.addMessageCreateListener(g);
-		helpListener.addHelpEmbed(g.getHelpEmbed());
+		//Greeter g = new Greeter(channelName);
+		//api.addMessageCreateListener(g);
+		//helpListener.addHelpEmbed(g.getHelpEmbed());
 
 		pythagcalc pythagCalc = new pythagcalc(channelName);
 		api.addMessageCreateListener(pythagCalc);
--- src/main/java/org/jointheleague/modules/_SammySamSam.java
+++ src/main/java/org/jointheleague/modules/_SammySamSam.java
@@ -1,19 +1,73 @@
 package org.jointheleague.modules;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
 import org.javacord.api.event.message.MessageCreateEvent;
 
 import net.aksingh.owmjapis.api.APIException;
 
 public class _SammySamSam extends CustomMessageCreateListener {
+	
+	public static final String COMMAND = "~add zoom";
+	
 	public _SammySamSam(String channelName) {
 		super(channelName);
 	}
-
+	
+	public String getUserInfo(String UserInfo) {
+		return "src/main/java/FilePackage/" + UserInfo + ".txt";
+	}
+	String fondou = getUserInfo("GrandPa");
 	@Override
 	public void handle(MessageCreateEvent event) throws APIException {
-		String m = event.getMessageContent();
-		if (m.toLowerCase().contains("~bingbongbing")) {
-			event.getChannel().sendMessage("Gaby is your supreme lord. He will vanquish you. You petty imbeciles");
+if (event.getMessageContent().contains(COMMAND)) {
+	//"~add zoom <name> <link>
+			String cmd = event.getMessageContent().substring(10); 
+			System.out.println(cmd);
+			String subject = cmd;
+			String link = cmd; 
+			subject = subject.substring(0, subject.indexOf(" "));
+			link = link.substring(link.indexOf(" ")+1, link.length());
+			
+		
+			
+			try {
+				FileWriter fw = new FileWriter(fondou, true);
+				String classex = subject + ":" + "\n" + "Link: " + link;
+				classex = classex.trim();
+				classex += "\n";
+				fw.write(classex);
+				fw.close();
+				System.out.println(classex);
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}	
+}
+
+try {
+	BufferedReader br;
+	br = new BufferedReader(new FileReader(fondou));
+	String line = br.readLine(); 
+	String candy = "";
+	while (line!=null) {
+		candy += line + "\n"; 
 		}
+	br.close();
+	event.getChannel().sendMessage(candy);
+	System.out.println(candy);
+} catch (FileNotFoundException e) {
+	// TODO Auto-generated catch block
+	e.printStackTrace();
+} catch (IOException e) {
+	// TODO Auto-generated catch block
+	e.printStackTrace();
+}
+
+
 	}
 }
