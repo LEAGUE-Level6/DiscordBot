a71b2d50259fb43a526dc5dcb5e8445618ad76a7
root <root@16424db850fb>
2021-04-22 01:28:10 +0000

    finished the 2d array, incorporate that into if statements

--- src/main/java/org/jointheleague/modules/CompareBrand.java
+++ src/main/java/org/jointheleague/modules/CompareBrand.java
@@ -1,21 +1,24 @@
 package org.jointheleague.modules;
 
 import org.javacord.api.event.message.MessageCreateEvent;
+import org.jointheleague.modules.pojo.HelpEmbed;
 
 import net.aksingh.owmjapis.api.APIException;
 
 public class CompareBrand extends CustomMessageCreateListener {
 
-	
+	private static final String COMMAND = "/compare";
 	
 	public CompareBrand(String channelName) {
 		super(channelName);
+		helpEmbed = new HelpEmbed(COMMAND, "Type /compare budget followed by the maximum to search within a budget, same with horse power and cost. To do multiple, do /compare all");
 	}
 
 	@Override
 	public void handle(MessageCreateEvent event) throws APIException {
 		// TODO Auto-generated method stub
-		
+		if(event.getMessageContent().toLowerCase().startsWith(COMMAND));
+		event.getChannel().sendMessage("hi");
 	}
 
 }
