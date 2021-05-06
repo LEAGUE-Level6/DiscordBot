f998fc3816e737d58cb4263530bdf85717db63de
root <root@e00db3143a89>
2021-05-06 01:32:02 +0000

    fix discord naming issue not today's me's problem

--- src/main/java/org/jointheleague/modules/CompareBrand.java
+++ src/main/java/org/jointheleague/modules/CompareBrand.java
@@ -6,13 +6,12 @@ import org.jointheleague.modules.pojo.HelpEmbed;
 import net.aksingh.owmjapis.api.APIException;
 
 public class CompareBrand extends CustomMessageCreateListener {
-	Car car = new Car("Acura ILX", 5, 201, 26100);
-	Car[] scars = {new Car("Acura ILX", 5, 201, 26100), new Car("Acura MDX", 7, 2090, 46900), new Car("Acura TLX", 5, 272, 37500), new Car("Acura RDX", 5, 272, 38400), 
+	Car[] cars = {new Car("Acura ILX", 5, 201, 26100), new Car("Acura MDX", 7, 2090, 46900), new Car("Acura TLX", 5, 272, 37500), new Car("Acura RDX", 5, 272, 38400), 
 				   new Car("Alfa Romeo Stelvio", 5, 280, 42350), new Car("Alfa Romeo Giulia", 5, 280, 42350),
 				   new Car("Audi Q3", 5, 228, 34000), new Car("Audi Q5", 5, 362, 43300), new Car("Audi Q7", 7, 335, 54950), new Car("Audi Q8", 5, 335, 68200), new Car("Audi E-Tron", 5, 402, 65900),  new Car("Audi A3", 5, 184, 33300), new Car("Audi A4", 5, 261, 39100), new Car("Audi A5", 5, 261, 44000), new Car("Audi A6", 5, 335, 54900), new Car("Audi A7", 5, 335, 69200), new Car("Audi A8", 5, 453, 86500),
 				   new Car("BMW X1", 5, 228, 35400), new Car("BMW X2", 5, 228, 36600), new Car("BMW X3", 5, 248, 43000), new Car("BMW X4", 5, 245, 51600), new Car("BMW X5", 5, 335, 59400), new Car("BMW X6", 5, 335, 65050), new Car("BMW X7", 5, 335, 74900), new Car("BMW 2", 5, 228, 35900)
 				   };
-	String possibilities;
+	String possibilities = "";
 	private static final String COMMAND = "/compare";
 	public CompareBrand(String channelName) {
 		super(channelName);
@@ -48,14 +47,19 @@ public class CompareBrand extends CustomMessageCreateListener {
 		boolean budgetMet = false;
 		boolean seatsMet = false;
 		for (int i = 0; i < cars.length; i++) {
-			if (hasBudget && hasSeats && hasHorsies) {
-				budgetMet = car.checkPrice(Integer.parseInt(car[i][3]), budget);
-				hpMet = car.checkPrice(Integer.parseInt(car[i][2]), horsepower);
-				seatsMet = car.checkPrice(Integer.parseInt(car[i][1]), seats);
-				if (budgetMet && hpMet && seatsMet) {
+			if (hasBudget) {
+				budgetMet = cars[i].checkPrice(budget);
+			}
+			if (hasHorsies) {
+				hpMet = cars[i].checkHp(horsepower);
+			}
+			if (hasSeats) {
+				seatsMet = cars[i].checkSeats(seats);
+			}
+				
+				if ((budgetMet || !hasBudget) && (hpMet || !hasHorsies) && (seatsMet || !hasSeats)) {
 					event.getChannel().sendMessage(cars[i] + " contends");
 				}
-			}
 		}
 
 	}
@@ -75,22 +79,22 @@ class Car {
 		this.name = name;
 	}
 
-	public boolean checkHp(int hp, int hpLimit) {
+	public boolean checkHp(int hpLimit) {
 		if (hp <= hpLimit) {
 			return true;
 		}
 		return false;
 	}
 
-	public boolean checkSeats(int seats, int seatLimit) {
+	public boolean checkSeats(int seatLimit) {
 		if (seats <= seatLimit) {
 			return true;
 		}
 		return false;
 	}
 
-	public boolean checkPrice(int price, int priceLimit) {
-		if (hp <= priceLimit) {
+	public boolean checkPrice(int priceLimit) {
+		if (price <= priceLimit) {
 			return true;
 		}
 		return false;
