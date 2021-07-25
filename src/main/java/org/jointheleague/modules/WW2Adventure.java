1ef725cade9b56a2fc201b227f194607c4528a2b
RobloxYesCaillouNo <jaydenkudo@gmail.com>
2021-07-25 12:40:06 -0700

    idek
    bruhhh

--- src/main/java/org/jointheleague/modules/WW2Adventure.java
+++ src/main/java/org/jointheleague/modules/WW2Adventure.java
@@ -72,7 +72,7 @@ public class WW2Adventure extends CustomMessageCreateListener {
 
 		if (menuState1 == 0) {
 			if (txt.equalsIgnoreCase("!Midway")) {
-				event.getChannel().sendMessage("Ok soldier, you will be deployed out in the pacific against japan!"
+				event.getChannel().sendMessage("Ok soldier, you will be deployed out in the pacific against japan! "
 						+ "to survive this battle, you must play a game! Say !deploy to move on");
 				menuState1++;
 			}
@@ -95,7 +95,7 @@ public class WW2Adventure extends CustomMessageCreateListener {
 
 			return;
 		}
-		System.out.println(txt + gameState);
+		// System.out.println(txt + gameState);
 		if (gameState == 0) {
 			event.getChannel().sendMessage("Alright, your first task is to spell Soldier backwards");
 			gameState++;
@@ -115,7 +115,7 @@ public class WW2Adventure extends CustomMessageCreateListener {
 
 			} else {
 				gameState++;
-				System.out.println(txt);
+				// System.out.println(txt);
 
 			}
 		} else if (gameState == 2) {
@@ -145,27 +145,28 @@ public class WW2Adventure extends CustomMessageCreateListener {
 				event.getChannel().sendMessage(build);
 				event.getChannel().sendMessage("Here's what the M1 looks like:");
 				event.getChannel().sendMessage(new File(image + "m1.jpg"));
-				
+
 				event.getChannel().type();
 				try {
 					Thread.sleep(1500);
-				} catch(InterruptedException e) {
+				} catch (InterruptedException e) {
 					e.printStackTrace();
 				}
-			event.getChannel().sendMessage("**"
-					+ "Alright, heres a hard question. 2 more of these hard questions till u win the battle!" + "**");
-			event.getChannel().sendMessage("What do many people call the Battle Of Normandy? Hint: it starts with a D");
-			gameState += 6;
+				event.getChannel().sendMessage(
+						"**" + "Alright, heres a hard question. 2 more of these hard questions till u win the battle!"
+								+ "**");
+				event.getChannel()
+						.sendMessage("What do many people call the Battle Of Normandy? Hint: it starts with a D");
+				gameState += 6;
 			} else {
-			event.getChannel().sendMessage("Sorry, you got the answer incorect. You have died. "
-					+ " \n The correct answer was M1 Garand. If you wanna try again type !BeginWW2Journey");
-			gameState = 0;
+				event.getChannel().sendMessage("Sorry, you got the answer incorect. You have died. "
+						+ " \n The correct answer was M1 Garand. If you wanna try again type !BeginWW2Journey");
+				gameState = 0;
 			}
 		}
 
-		 else if (gameState == 9) {
-			if (txt.equalsIgnoreCase("DDay") || txt.equals("invasion of normandy") || txt.equalsIgnoreCase("D-Day")
-					) {
+		else if (gameState == 9) {
+			if (txt.equalsIgnoreCase("DDay") || txt.equals("invasion of normandy") || txt.equalsIgnoreCase("D-Day")) {
 				event.getChannel().sendMessage("Great job!");
 				event.getChannel().type();
 				build = new EmbedBuilder();
@@ -179,7 +180,7 @@ public class WW2Adventure extends CustomMessageCreateListener {
 				event.getChannel().sendMessage(build);
 				event.getChannel().sendMessage("**" + "Final Question. If you get this right then you win!" + "**");
 				event.getChannel().sendMessage("Lets see if you were paying attention: What gun was used in WW2 alot?");
-				gameState+=2;
+				gameState += 2;
 			} else {
 				event.getChannel().sendMessage("Sorry, you got the answer incorect. You have died. "
 						+ " \n The correct answer was D-Day. If you wanna try again type !BeginWW2Journey");
@@ -188,16 +189,16 @@ public class WW2Adventure extends CustomMessageCreateListener {
 			}
 
 		}
-	
-		 else if (gameState == 11) {
+
+		else if (gameState == 11) {
 			if (txt.equals("M1 garand") || txt.equals("M1") || txt.equals("garand") || txt.equals("M1 Garand")
 					|| txt.equals("m1 garand")) {
 				event.getChannel().sendMessage("Awesome job! You survived this battle!");
-				event.getChannel()
-					.sendMessage("If you want to start again you could do one of the battles listed above, or if you're"
-							+ "done then type !go home, or say !continue if you want to continue");
-				
-				gameState+=2;
+				event.getChannel().sendMessage(
+						"If you want to start again you could do one of the battles listed above, or if you're"
+								+ "done then type !go home, or say !continue if you want to continue");
+
+				gameState += 2;
 			} else {
 				event.getChannel().sendMessage(
 						"Sorry, you got the answer incorect. You have died when you almost won the battle. "
@@ -206,8 +207,8 @@ public class WW2Adventure extends CustomMessageCreateListener {
 
 			}
 		}
-		 
-		 else if (gameState == 13) {
+
+		else if (gameState == 13) {
 			if (txt.equals("!go home")) {
 				event.getChannel().sendMessage("Congrats soldier you have made it home safely!");
 
@@ -225,19 +226,31 @@ public class WW2Adventure extends CustomMessageCreateListener {
 	}
 
 	public void middway(MessageCreateEvent event) {
-
+		if (event.getMessageAuthor().isBotUser()) {
+			return;
+		}
 		String txt = event.getMessageContent();
 		EmbedBuilder build = new EmbedBuilder();
-		event.getChannel().sendMessage("Here is your first question: ");
-		gameState1++;
-		if (gameState1 == 3) {
+		if (txt.isEmpty()) {
+			return;
+		}
+		if (gameState1 == 0) {
+			event.getChannel().sendMessage("Here is your first question: ");
+			event.addReactionsToMessage("😂");
+
 			event.getChannel().sendMessage("Spell Japan backwards");
 			gameState1++;
-		}
-		if (gameState1 >= 4) {
-			if (txt.equals("napaJ") || txt.equals("napaj")) {
+		} else if (gameState1 == 1) {
+
+			if (txt.equalsIgnoreCase("napaJ")) {
 				event.getChannel().sendMessage("Great work soldier");
 				event.getChannel().type();
+				try {
+					Thread.sleep(1000);
+				} catch (InterruptedException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
 				build = new EmbedBuilder();
 				build.setThumbnail("https://i.pinimg.com/originals/79/7f/7c/797f7c26a67cfb1c691ccaa7d540df24.jpg");
 				build.setColor(Color.red).setTitle("Cool stuff about the battle of midway").addField(
@@ -248,19 +261,27 @@ public class WW2Adventure extends CustomMessageCreateListener {
 						"The USA would do something known as \"Island Hopping\" which refers to them battling against japan "
 								+ "\n and then setting up their bases on each island, helping them to progress closer to japan, towards victory");
 				event.getChannel().sendMessage(build);
-				gameState1++;
+				event.getChannel().type();
+				try {
+					Thread.sleep(1000);
+				} catch (InterruptedException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				event.getChannel().sendMessage("Here is your next question: ");
+				event.getChannel().type();
+				event.getChannel().sendMessage("What ocean did the battle of midway happen in?");
+				gameState1 += 3;
 			} else {
-				event.getChannel().sendMessage(
-						"Sorry soldier, you have died! The correct answer was napaJ. Type !BeginWW2Journey if you wanna start over.");
-				gameState1 = 0;
 
+				gameState1++;
 			}
 
-		}
-		if (gameState1 == 5) {
-			event.getChannel().sendMessage("Here is your next question: ");
-			event.getChannel().type();
-			event.getChannel().sendMessage("What ocean did the battle of midway happen in?");
+		} else if (gameState1 == 2) {
+			event.getChannel().sendMessage(
+					"Sorry soldier, you have died! The correct answer was napaJ. Type !BeginWW2Journey if you wanna start over.");
+			gameState1 = 0;
+		} else if (gameState1 == 5) {
 			if (txt.equalsIgnoreCase("Pacific") || txt.equalsIgnoreCase("Pacific ocean")) {
 				event.getChannel().sendMessage("Nice Job soldier!!");
 				build.setThumbnail(
@@ -271,47 +292,52 @@ public class WW2Adventure extends CustomMessageCreateListener {
 				build.addField("Another Fact: ",
 						"After the US won this battle, victory for Japan and the axis were no longer able to happen");
 				event.getChannel().sendMessage(build);
+
 				gameState1++;
 			} else {
 				event.getChannel().sendMessage("You have died, the Correct answer is pacific ocean");
 				gameState1 = 0;
 
 			}
-		}
-		if (gameState1 == 6) {
+		} else if (gameState1 == 6) {
 			event.getChannel().type();
 			event.getChannel().sendMessage("Your next question is: /n spell Midway in alphabetical order");
 			if (txt.equalsIgnoreCase("adiMwy")) {
 				event.getChannel().sendMessage("Nice one!");
+				try {
+					Thread.sleep(1000);
+				} catch (InterruptedException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				event.getChannel().type();
+				event.getChannel().sendMessage(
+						"Here is your final question. This will be tough, but if you get it right then you win this battle!");
+				build = new EmbedBuilder();
+				build.setTitle("Your final question (true or false): is this midway island?");
+				build.setColor(Color.gray);
+				build.setThumbnail("https://web.mst.edu/~rogersda/military_service/Eastern%20Island.jpg");
+				event.getChannel().sendMessage(build);
 				gameState1++;
 			} else {
 				event.getChannel().sendMessage("Sorry you have died. The correct answer is adiMwy.");
 				gameState = 0;
 
 			}
-		}
-		if (gameState1 == 7) {
-			event.getChannel().type();
-			event.getChannel().sendMessage(
-					"Here is your final question. This will be tough, but if you get it right then you win this battle!");
-			build = new EmbedBuilder();
-			build.setTitle("Your final question (true or false): is this midway island?");
-			build.setColor(Color.gray);
-			build.setThumbnail("https://web.mst.edu/~rogersda/military_service/Eastern%20Island.jpg");
-			event.getChannel().sendMessage(build);
-		}
-		if (txt.equalsIgnoreCase("true") || txt.equalsIgnoreCase("yes")) {
-			event.getChannel().sendMessage("WWOOO U GOT IT BRO!");
-			event.getChannel().type();
-			event.getChannel().sendMessage("If you wanna play again type !BeginWW2Journey. If you want");
-			gameState1 = 0;
+		} else if (gameState1 == 7) {
+			if (txt.equalsIgnoreCase("true") || txt.equalsIgnoreCase("yes")) {
+				event.getChannel().sendMessage("WWOOO U GOT IT BRO!");
+				event.getChannel().type();
+				event.getChannel().sendMessage("If you wanna play again type !BeginWW2Journey. If you want");
+				gameState1 = 0;
+				menuState1 = 0;
 
-		} else {
-			event.getChannel().sendMessage("Sorry you have died. The correct answer is true.");
-			gameState1 = 0;
+			} else {
+				event.getChannel().sendMessage("Sorry you have died. The correct answer is true.");
+				gameState1 = 0;
 
+			}
 		}
-
 	}
 
 }
