d2ee54727d05c030a6a93214c4d919d931ea9c5b
gabrielsong9674 <gracegabrielson@icloud.com>
2021-07-25 19:55:48 -0500

    guess age feature started

---
+++ src/main/java/org/jointheleague/modules/GuessAge.java
@@ -0,0 +1,37 @@
+package org.jointheleague.modules;
+
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.ProtocolException;
+import java.net.URL;
+
+import javax.json.Json;
+import javax.json.JsonObject;
+import javax.json.JsonReader;
+
+public class GuessAge {
+	
+	public int getAgeWithName(String name) {
+	    String requestURL = "https://api.agify.io?name= "+name;
+	    try {
+			URL url = new URL(requestURL);
+			HttpURLConnection con = (HttpURLConnection) url.openConnection();
+			con.setRequestMethod("GET");
+			JsonReader repoReader = Json.createReader(con.getInputStream());
+		    JsonObject userJSON = ((JsonObject) repoReader.read());
+		    con.disconnect();
+	    }
+	    catch(MalformedURLException e){
+			e.printStackTrace();
+	    } catch (ProtocolException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	    return 0;
+	}
+}
+
