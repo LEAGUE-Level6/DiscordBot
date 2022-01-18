83ef7b75887a9871cab93b1dc5d9cc5c42e40ee7
Roshan Sood <legodude2774@gmail.com>
2022-01-17 17:11:28 -0800

    Updated the first feature

--- src/main/java/org/jointheleague/modules/NumberRandom.java
+++ src/main/java/org/jointheleague/modules/NumberRandom.java
@@ -6,7 +6,7 @@ import org.javacord.api.event.message.MessageCreateEvent;
 
 import java.util.Random;
 
-public class NumberRandom extends CustomMessageCreateListener{
+public class NumberRandom extends CustomMessageCreateListener {
     public NumberRandom(String channelName) {
         super(channelName);
     }
@@ -18,6 +18,18 @@ public class NumberRandom extends CustomMessageCreateListener{
         if (messageContent.startsWith(COMMAND)) {
             Random r = new Random();
             event.getChannel().sendMessage("Your random number is " + r.nextInt(1001));
+        }
+
+        String input = "!game";
+        String input2 = "higher";
+        String input3 = "lower";
+        String messageContent2 = event.getMessageContent();
+        if (messageContent2.startsWith(input)) {
+            Random r2 = new Random();
+            int ran = r2.nextInt(1001);
+            event.getChannel().sendMessage("Welcome to higher or lower. Your guess: " + ran);
+
+
         }
     }
 }
